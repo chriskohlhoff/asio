c8aab52f0c955b0976cf1308867d66faa7810603
chris <chris>
2003-10-09 02:56:35 +0000

    Added documentation for the error codes.

--- asio/include/asio/socket_error.hpp
+++ asio/include/asio/socket_error.hpp
@@ -41,40 +41,102 @@ public:
   /// Error codes.
   enum
   {
+    /// Permission denied.
     access_denied = ASIO_SOCKET_ERROR(EACCES),
+
+    /// Address family not supported by protocol.
     address_family_not_supported = ASIO_SOCKET_ERROR(EAFNOSUPPORT),
+
+    /// Address already in use.
     address_in_use = ASIO_SOCKET_ERROR(EADDRINUSE),
+
+    /// Transport endpoint is already connected.
     already_connected = ASIO_SOCKET_ERROR(EISCONN),
+
+    /// Operation already in progress.
     already_started = ASIO_SOCKET_ERROR(EALREADY),
+
+    /// Connection refused.
     connection_refused = ASIO_SOCKET_ERROR(ECONNREFUSED),
+
+    /// Connection reset by peer.
     connection_reset = ASIO_SOCKET_ERROR(ECONNRESET),
+
+    /// Bad file descriptor.
     bad_descriptor = ASIO_SOCKET_ERROR(EBADF),
+
+    /// Bad address.
     fault = ASIO_SOCKET_ERROR(EFAULT),
+
+    /// No route to host.
     host_unreachable = ASIO_SOCKET_ERROR(EHOSTUNREACH),
+
+    /// Operation now in progress.
     in_progress = ASIO_SOCKET_ERROR(EINPROGRESS),
+
+    /// Interrupted system call.
     interrupted = ASIO_SOCKET_ERROR(EINTR),
+
+    /// Invalid argument.
     invalid_argument = ASIO_SOCKET_ERROR(EINVAL),
+
+    /// Message too long.
     message_size = ASIO_SOCKET_ERROR(EMSGSIZE),
+
+    /// Network is down.
     network_down = ASIO_SOCKET_ERROR(ENETDOWN),
+
+    /// Network dropped connection on reset.
     network_reset = ASIO_SOCKET_ERROR(ENETRESET),
+
+    /// Network is unreachable.
     network_unreachable = ASIO_SOCKET_ERROR(ENETUNREACH),
+
+    /// Too many open files.
     no_descriptors = ASIO_SOCKET_ERROR(EMFILE),
+
+    /// No buffer space available.
     no_buffer_space = ASIO_SOCKET_ERROR(ENOBUFS),
+
+    /// Cannot allocate memory.
     no_memory = ENOMEM,
+
+    /// Operation not permitted.
     no_permission = EPERM,
+
+    /// Protocol not available.
     no_protocol_option = ASIO_SOCKET_ERROR(ENOPROTOOPT),
+
+    /// Transport endpoint is not connected.
     not_connected = ASIO_SOCKET_ERROR(ENOTCONN),
+
+    /// Socket operation on non-socket.
     not_socket = ASIO_SOCKET_ERROR(ENOTSOCK),
+
+    /// Operation not supported.
     not_supported = ASIO_SOCKET_ERROR(EOPNOTSUPP),
+
+    /// Operation cancelled.
 #if defined(_WIN32)
     operation_aborted = ERROR_OPERATION_ABORTED,
 #else // defined(_WIN32)
     operation_aborted = ECANCELED,
 #endif // defined(_WIN32)
+
+    /// Cannot send after transport endpoint shutdown.
     shut_down = ASIO_SOCKET_ERROR(ESHUTDOWN),
+
+    /// Success.
     success = 0,
+
+    /// Connection timed out.
     timed_out = ASIO_SOCKET_ERROR(ETIMEDOUT),
+
+    /// Resource temporarily unavailable.
     try_again = EAGAIN,
+
+    /// The socket is marked non-blocking and the requested operation would
+    /// block.
     would_block = ASIO_SOCKET_ERROR(EWOULDBLOCK)
   };
 
