8d4839f4f9dca529f668f3e83af396d3277692e5
chris <chris>
2004-01-06 07:26:27 +0000

    Added copyright notices.

--- asio/src/tests/performance/client.cpp
+++ asio/src/tests/performance/client.cpp
@@ -1,3 +1,17 @@
+//
+// client.hpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003, 2004 Christopher M. Kohlhoff (chris@kohlhoff.com)
+//
+// Permission to use, copy, modify, distribute and sell this software and its
+// documentation for any purpose is hereby granted without fee, provided that
+// the above copyright notice appears in all copies and that both the copyright
+// notice and this permission notice appear in supporting documentation. This
+// software is provided "as is" without express or implied warranty, and with
+// no claim as to its suitability for any purpose.
+//
+
 #include "asio.hpp"
 #include <algorithm>
 #include <boost/bind.hpp>
