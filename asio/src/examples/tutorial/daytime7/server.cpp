53f06acc1e668f3c8756fb7c4438bd7f16af012e
chris <chris>
2004-05-10 09:05:57 +0000

    Added a combined TCP/UDP daytime server example.

---
+++ asio/src/examples/tutorial/daytime7/server.cpp
@@ -0,0 +1,95 @@
+#include <ctime>
+#include <iostream>
+#include "boost/bind.hpp"
+#include "asio.hpp"
+
+void handle_tcp_send(asio::stream_socket* socket, char* send_buf)
+{
+  using namespace std; // For free.
+  free(send_buf);
+  delete socket;
+}
+
+void handle_tcp_accept(asio::socket_acceptor* acceptor,
+    asio::stream_socket* socket, const asio::socket_error& error)
+{
+  if (!error)
+  {
+    using namespace std; // For time_t, time, ctime, strdup and strlen.
+    time_t now = time(0);
+    char* send_buf = strdup(ctime(&now));
+    size_t send_length = strlen(send_buf);
+
+    asio::async_send_n(*socket, send_buf, send_length,
+        boost::bind(handle_tcp_send, socket, send_buf));
+
+    socket = new asio::stream_socket(acceptor->demuxer());
+
+    acceptor->async_accept(*socket,
+        boost::bind(handle_tcp_accept, acceptor, socket, asio::arg::error));
+  }
+  else
+  {
+    delete socket;
+  }
+}
+
+void handle_udp_sendto(char* send_buf)
+{
+  using namespace std; // For free.
+  free(send_buf);
+}
+
+void handle_udp_recvfrom(asio::dgram_socket* socket, char* recv_buf,
+    size_t recv_length, asio::ipv4::address* remote_address,
+    const asio::socket_error& error)
+{
+  if (!error || error == asio::socket_error::message_size)
+  {
+    using namespace std; // For time_t, time, ctime, strdup and strlen.
+    time_t now = time(0);
+    char* send_buf = strdup(ctime(&now));
+    size_t send_length = strlen(send_buf);
+
+    socket->async_sendto(send_buf, send_length, *remote_address,
+        boost::bind(handle_udp_sendto, send_buf));
+
+    socket->async_recvfrom(recv_buf, recv_length, *remote_address,
+        boost::bind(handle_udp_recvfrom, socket, recv_buf, recv_length,
+          remote_address, asio::arg::error));
+  }
+}
+
+int main()
+{
+  try
+  {
+    asio::demuxer demuxer;
+
+    asio::socket_acceptor tcp_acceptor(demuxer, asio::ipv4::address(13));
+
+    asio::stream_socket* tcp_socket = new asio::stream_socket(demuxer);
+
+    tcp_acceptor.async_accept(*tcp_socket,
+        boost::bind(handle_tcp_accept, &tcp_acceptor, tcp_socket,
+          asio::arg::error));
+
+    asio::dgram_socket udp_socket(demuxer, asio::ipv4::address(13));
+
+    char recv_buf[1];
+    size_t recv_length = sizeof(recv_buf);
+    asio::ipv4::address remote_address;
+
+    udp_socket.async_recvfrom(recv_buf, recv_length, remote_address,
+        boost::bind(handle_udp_recvfrom, &udp_socket, recv_buf, recv_length,
+          &remote_address, asio::arg::error));
+
+    demuxer.run();
+  }
+  catch (asio::socket_error& e)
+  {
+    std::cerr << e.what() << ": " << e.message() << std::endl;
+  }
+
+  return 0;
+}
