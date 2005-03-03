be9d0635800957ec9d54e490bfe403bac913bff6
chris_kohlhoff <chris_kohlhoff>
2005-03-03 05:22:00 +0000

    Update copyright strings to include 2005 (Sourceforge Request ID 1154770).

--- asio/include/asio/detail/hash_map.hpp
+++ asio/include/asio/detail/hash_map.hpp
@@ -2,7 +2,7 @@
 // hash_map.hpp
 // ~~~~~~~~~~~~
 //
-// Copyright (c) 2003, 2004 Christopher M. Kohlhoff (chris@kohlhoff.com)
+// Copyright (c) 2003-2005 Christopher M. Kohlhoff (chris@kohlhoff.com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
