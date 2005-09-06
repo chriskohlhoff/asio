4a7d22dc56efb25c7b01ee631739011ddfb244e4
chris_kohlhoff <chris_kohlhoff>
2005-09-06 11:29:13 +0000

    Ensure that WSAStartup/WSACleanup are only called once, as they can leak on some versions of windows.

--- asio/include/asio/detail/winsock_init.hpp
+++ asio/include/asio/detail/winsock_init.hpp
@@ -19,6 +19,11 @@
 
 #if defined(_WIN32)
 
+#include "asio/detail/push_options.hpp"
+#include <boost/noncopyable.hpp>
+#include <boost/shared_ptr.hpp>
+#include "asio/detail/pop_options.hpp"
+
 #include "asio/detail/socket_types.hpp"
 
 namespace asio {
@@ -26,21 +31,59 @@ namespace detail {
 
 template <int Major = 2, int Minor = 0>
 class winsock_init
+  : private boost::noncopyable
 {
+private:
+  // Structure to perform the actual initialisation.
+  struct do_init
+  {
+    do_init()
+    {
+      WSADATA wsa_data;
+      ::WSAStartup(MAKEWORD(Major, Minor), &wsa_data);
+    }
+
+    ~do_init()
+    {
+      ::WSACleanup();
+    }
+
+    // Helper function to manage a do_init singleton. The static instance of the
+    // winsock_init object ensures that this function is always called before
+    // main, and therefore before any other threads can get started. The do_init
+    // instance must be static in this function to ensure that it gets
+    // initialised before any other global objects try to use it.
+    static boost::shared_ptr<do_init> instance()
+    {
+      static boost::shared_ptr<do_init> init(new do_init);
+      return init;
+    }
+  };
+
 public:
   // Constructor.
   winsock_init()
+    : ref_(do_init::instance())
   {
-    WSADATA wsa_data;
-    ::WSAStartup(MAKEWORD(Major, Minor), &wsa_data);
+    while (&instance_ == 0); // Ensure winsock_init::instance_ is linked in.
   }
 
   // Destructor.
   ~winsock_init()
   {
-    ::WSACleanup();
   }
+
+private:
+  // Instance to force initialisation of winsock at global scope.
+  static winsock_init instance_;
+
+  // Reference to singleton do_init object to ensure that winsock does not get
+  // cleaned up until the last user has finished with it.
+  boost::shared_ptr<do_init> ref_;
 };
+
+template <int Major, int Minor>
+winsock_init<Major, Minor> winsock_init<Major, Minor>::instance_;
 
 } // namespace detail
 } // namespace asio
