97bcbbf37b775e2b6636828187738a7555dc1b15
chris_kohlhoff <chris_kohlhoff>
2005-09-25 12:22:23 +0000

    Add missing derivation from boost::noncopyable.

--- asio/include/asio/basic_locking_dispatcher.hpp
+++ asio/include/asio/basic_locking_dispatcher.hpp
@@ -43,6 +43,7 @@ namespace asio {
  */
 template <typename Service>
 class basic_locking_dispatcher
+  : private boost::noncopyable
 {
 public:
   /// The type of the service that will be used to provide locking dispatcher
