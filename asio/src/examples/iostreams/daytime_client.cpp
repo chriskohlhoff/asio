7ae16c2fa8579304743a81c56e13fd0ad9ba4236
chris_kohlhoff <chris_kohlhoff>
2005-10-15 03:06:18 +0000

    Disable iostreams example for gcc 4.0.2.

--- asio/src/examples/iostreams/daytime_client.cpp
+++ asio/src/examples/iostreams/daytime_client.cpp
@@ -1,3 +1,11 @@
+// This example is disabled for gcc 4.0.2 because Boost.Iostreams and that
+// compiler version don't seem to get along very well.
+#if defined(__GNUC__) \
+  && (__GNUC__ == 4 && __GNUC_MINOR__ == 0 && __GNUC_PATCHLEVEL__ == 2)
+#warning iostreams example is disabled for gcc 4.0.2
+int main() {}
+#else
+
 #include <iostream>
 #include <string>
 #include "socket_stream.hpp"
@@ -24,3 +32,5 @@ int main(int argc, char* argv[])
 
   return 0;
 }
+
+#endif
