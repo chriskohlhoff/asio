bdc4bda5032c9af1ed19957f4633da566e2d0bd4
chris_kohlhoff <chris_kohlhoff>
2005-11-20 04:36:46 +0000

    Obfuscate email addresses to prevent harvesting for spam.

--- asio/include/asio/ssl/detail/openssl_operation.hpp
+++ asio/include/asio/ssl/detail/openssl_operation.hpp
@@ -2,7 +2,7 @@
 // openssl_operation.hpp
 // ~~~~~~~~~~~~~~~~~~~~~
 //
-// Copyright (c) 2005 Voipster / Indrek.Juhani@voipster.com
+// Copyright (c) 2005 Voipster / Indrek dot Juhani at voipster dot com
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
--- asio/include/asio/ssl/stream_service.hpp
+++ asio/include/asio/ssl/stream_service.hpp
@@ -2,8 +2,8 @@
 // stream_service.hpp
 // ~~~~~~~~~~~~~~~~~~
 //
-// Copyright (c) 2005 Voipster / Indrek.Juhani@voipster.com
-// Copyright (c) 2005 Christopher M. Kohlhoff (chris@kohlhoff.com)
+// Copyright (c) 2005 Voipster / Indrek dot Juhani at voipster dot com
+// Copyright (c) 2005 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
