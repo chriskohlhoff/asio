0cba7a3678927749f0cd1dcb0405c413537b2faa
chris_kohlhoff <chris_kohlhoff>
2006-05-28 08:19:37 +0000

    Define BOOST_ALL_NO_LIB in unit tests to make it easier to build them on their own.

--- asio/src/tests/unit/ssl/context_service_test.cpp
+++ asio/src/tests/unit/ssl/context_service_test.cpp
@@ -8,6 +8,11 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+// Disable autolinking for unit tests.
+#if !defined(BOOST_ALL_NO_LIB)
+#define BOOST_ALL_NO_LIB 1
+#endif // !defined(BOOST_ALL_NO_LIB)
+
 // Test that header file is self-contained.
 #include "asio/ssl/context_service.hpp"
 
