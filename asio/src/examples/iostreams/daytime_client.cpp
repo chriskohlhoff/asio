5f39b8755d7a9197d6b3de22dcad910eb014c050
chris_kohlhoff <chris_kohlhoff>
2006-06-13 22:51:17 +0000

    Update iostreams example to use new ip::tcp::iostream class.

--- asio/src/examples/iostreams/daytime_client.cpp
+++ asio/src/examples/iostreams/daytime_client.cpp
@@ -1,14 +1,6 @@
-// This example is disabled for gcc 4.0.2 because Boost.Iostreams and that
-// compiler version don't seem to get along very well.
-#if defined(__GNUC__) \
-  && (__GNUC__ == 4 && __GNUC_MINOR__ == 0 && __GNUC_PATCHLEVEL__ == 2)
-#warning iostreams example is disabled for gcc 4.0.2
-int main() {}
-#else
-
 #include <iostream>
 #include <string>
-#include "socket_stream.hpp"
+#include <asio.hpp>
 
 int main(int argc, char* argv[])
 {
@@ -20,7 +12,7 @@ int main(int argc, char* argv[])
       return 1;
     }
 
-    socket_stream s(argv[1], "daytime");
+    asio::ip::tcp::iostream s(argv[1], "daytime");
     std::string line;
     std::getline(s, line);
     std::cout << line << std::endl;
@@ -32,5 +24,3 @@ int main(int argc, char* argv[])
 
   return 0;
 }
-
-#endif
