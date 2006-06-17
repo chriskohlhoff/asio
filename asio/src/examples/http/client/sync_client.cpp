81318ad10878058c20f8ee3acd29b379f7f4b753
chris_kohlhoff <chris_kohlhoff>
2006-06-17 14:15:54 +0000

    Add HTTP client examples that use read_until/async_read_until.

---
+++ asio/src/examples/http/client/sync_client.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <string>
+#include <asio.hpp>
+#include <boost/regex.hpp>
+
+using asio::ip::tcp;
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 3)
+    {
+      std::cout << "Usage: sync_client <server> <path>\n";
+      std::cout << "Example:\n";
+      std::cout << "  sync_client www.boost.org /LICENSE_1_0.txt\n";
+      return 1;
+    }
+
+    asio::io_service io_service;
+
+    // Get a list of endpoints corresponding to the server name.
+    tcp::resolver resolver(io_service);
+    tcp::resolver::query query(argv[1], "http");
+    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+    tcp::resolver::iterator end;
+
+    // Try each endpoint until we successfully establish a connection.
+    tcp::socket socket(io_service);
+    asio::error error = asio::error::host_not_found;
+    while (error && endpoint_iterator != end)
+    {
+      socket.close();
+      socket.connect(*endpoint_iterator++, asio::assign_error(error));
+    }
+    if (error)
+      throw error;
+
+    // Form the request. We specify the "Connection: close" header so that the
+    // server will close the socket after transmitting the response. This will
+    // allow us to treat all data up until the EOF as the content.
+    asio::streambuf request;
+    std::ostream request_stream(&request);
+    request_stream << "GET " << argv[2] << " HTTP/1.0\r\n";
+    request_stream << "Host: " << argv[1] << "\r\n";
+    request_stream << "Accept: */*\r\n";
+    request_stream << "Connection: close\r\n\r\n";
+
+    // Send the request.
+    asio::write(socket, request);
+
+    // Read the response status line.
+    asio::streambuf response;
+    asio::read_until(socket, response, boost::regex("\r\n"));
+
+    // Check that response is OK.
+    std::istream response_stream(&response);
+    std::string http_version;
+    response_stream >> http_version;
+    unsigned int status_code;
+    response_stream >> status_code;
+    std::string status_message;
+    std::getline(response_stream, status_message);
+    if (!response_stream || http_version.substr(0, 5) != "HTTP/")
+    {
+      std::cout << "Invalid response\n";
+      return 1;
+    }
+    if (status_code != 200)
+    {
+      std::cout << "Response returned with status code " << status_code << "\n";
+      return 1;
+    }
+
+    // Read the response headers, which are terminated by a blank line.
+    asio::read_until(socket, response, boost::regex("\r\n\r\n"));
+
+    // Process the response headers.
+    std::string header;
+    while (std::getline(response_stream, header) && header != "\r")
+      std::cout << header << "\n";
+    std::cout << "\n";
+
+    // Write whatever content we already have to output.
+    if (response.size() > 0)
+      std::cout << &response;
+
+    // Read until EOF, writing data to output as we go.
+    while (asio::read(socket, response,
+          asio::transfer_at_least(1),
+          asio::assign_error(error)))
+      std::cout << &response;
+    if (error != asio::error::eof)
+      throw error;
+  }
+  catch (std::exception& e)
+  {
+    std::cout << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
+}
