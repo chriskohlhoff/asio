19c52b885c92eaf1ec65d07b9e51bb2bb9c65bbf
chris_kohlhoff <chris_kohlhoff>
2006-07-19 22:39:16 +0000

    Don't wrap the time_t time value in a class, just the duration.

--- asio/src/examples/timers/time_t_timer.cpp
+++ asio/src/examples/timers/time_t_timer.cpp
@@ -5,12 +5,7 @@
 struct time_t_traits
 {
   // The time type.
-  struct time_type
-  {
-    time_type() : value(0) {}
-    time_type(std::time_t v) : value(v) {}
-    std::time_t value;
-  };
+  typedef std::time_t time_type;
 
   // The duration type.
   struct duration_type
@@ -29,19 +24,19 @@ struct time_t_traits
   // Add a duration to a time.
   static time_type add(const time_type& t, const duration_type& d)
   {
-    return t.value + d.value;
+    return t + d.value;
   }
 
   // Subtract one time from another.
   static duration_type subtract(const time_type& t1, const time_type& t2)
   {
-    return duration_type(t1.value - t2.value);
+    return duration_type(t1 - t2);
   }
 
   // Test whether one time is less than another.
   static bool less_than(const time_type& t1, const time_type& t2)
   {
-    return t1.value < t2.value;
+    return t1 < t2;
   }
 
   // Convert to POSIX duration type.
