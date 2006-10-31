5dc17dc5b0b8723b45b913d3aa3fe3cd8091b6ef
chris_kohlhoff <chris_kohlhoff>
2006-10-31 06:07:41 +0000

    Add empty unit test for read_until.

---
+++ asio/src/tests/unit/read_until_test.cpp
@@ -0,0 +1,25 @@
+//
+// read_until_test.cpp
+// ~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2006 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+// Disable autolinking for unit tests.
+#if !defined(BOOST_ALL_NO_LIB)
+#define BOOST_ALL_NO_LIB 1
+#endif // !defined(BOOST_ALL_NO_LIB)
+
+// Test that header file is self-contained.
+#include "asio/read_until.hpp"
+
+#include "unit_test.hpp"
+
+test_suite* init_unit_test_suite(int argc, char* argv[])
+{
+  test_suite* test = BOOST_TEST_SUITE("read_until");
+  return test;
+}
