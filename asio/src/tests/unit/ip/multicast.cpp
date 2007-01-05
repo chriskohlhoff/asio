75ea719c9d0f6d9bf193022aec5d0d19c4702516
chris_kohlhoff <chris_kohlhoff>
2007-01-05 05:37:41 +0000

    Test multicast socket options with both IPv4 and IPv6.

--- asio/src/tests/unit/ip/multicast.cpp
+++ asio/src/tests/unit/ip/multicast.cpp
@@ -108,81 +108,190 @@ using namespace asio;
 void test()
 {
   io_service ios;
-  ip::udp::endpoint ep(ip::address_v4::any(), 0);
-  ip::udp::socket sock(ios, ep);
-  const ip::address multicast_address = ip::address::from_string("239.255.0.1");
   asio::error_code ec;
+
+  ip::udp::endpoint ep_v4(ip::address_v4::any(), 0);
+  ip::udp::socket sock_v4(ios);
+  sock_v4.open(ep_v4.protocol(), ec);
+  bool have_v4 = !ec;
+  sock_v4.bind(ep_v4);
+
+  ip::udp::endpoint ep_v6(ip::address_v6::any(), 0);
+  ip::udp::socket sock_v6(ios);
+  sock_v6.open(ep_v6.protocol(), ec);
+  bool have_v6 = !ec;
+  sock_v6.bind(ep_v6);
+
+  BOOST_CHECK(have_v4 || have_v6);
+
+  const ip::address multicast_address_v4 =
+    ip::address::from_string("239.255.0.1", ec);
+  BOOST_CHECK(!have_v4 || !ec);
+
+  const ip::address multicast_address_v6 =
+    ip::address::from_string("ff31::8000:1234", ec);
+  BOOST_CHECK(!have_v6 || !ec);
 
   // join_group class.
 
-  ip::multicast::join_group join_group(multicast_address);
-  sock.set_option(join_group, ec);
-  BOOST_CHECK(!ec);
+  if (have_v4)
+  {
+    ip::multicast::join_group join_group(multicast_address_v4);
+    sock_v4.set_option(join_group, ec);
+    BOOST_CHECK(!ec);
+  }
+
+  if (have_v6)
+  {
+    ip::multicast::join_group join_group(multicast_address_v6);
+    sock_v6.set_option(join_group, ec);
+    BOOST_CHECK(!ec);
+  }
 
   // leave_group class.
 
-  ip::multicast::leave_group leave_group(multicast_address);
-  sock.set_option(leave_group, ec);
-  BOOST_CHECK(!ec);
+  if (have_v4)
+  {
+    ip::multicast::leave_group leave_group(multicast_address_v4);
+    sock_v4.set_option(leave_group, ec);
+    BOOST_CHECK(!ec);
+  }
+
+  if (have_v6)
+  {
+    ip::multicast::leave_group leave_group(multicast_address_v6);
+    sock_v6.set_option(leave_group, ec);
+    BOOST_CHECK(!ec);
+  }
 
   // outbound_interface class.
 
-  ip::multicast::outbound_interface outbound_interface(
-      ip::address_v4::loopback());
-  sock.set_option(outbound_interface, ec);
-  BOOST_CHECK(!ec);
+  if (have_v4)
+  {
+    ip::multicast::outbound_interface outbound_interface(
+        ip::address_v4::loopback());
+    sock_v4.set_option(outbound_interface, ec);
+    BOOST_CHECK(!ec);
+  }
+
+  if (have_v6)
+  {
+    ip::multicast::outbound_interface outbound_interface(1);
+    sock_v6.set_option(outbound_interface, ec);
+    BOOST_CHECK(!ec);
+  }
 
   // hops class.
 
-  ip::multicast::hops hops1(1);
-  BOOST_CHECK(hops1.value() == 1);
-  sock.set_option(hops1, ec);
-  BOOST_CHECK(!ec);
+  if (have_v4)
+  {
+    ip::multicast::hops hops1(1);
+    BOOST_CHECK(hops1.value() == 1);
+    sock_v4.set_option(hops1, ec);
+    BOOST_CHECK(!ec);
 
-  ip::multicast::hops hops2;
-  sock.get_option(hops2, ec);
-  BOOST_CHECK(!ec);
-  BOOST_CHECK(hops2.value() == 1);
+    ip::multicast::hops hops2;
+    sock_v4.get_option(hops2, ec);
+    BOOST_CHECK(!ec);
+    BOOST_CHECK(hops2.value() == 1);
 
-  ip::multicast::hops hops3(0);
-  BOOST_CHECK(hops3.value() == 0);
-  sock.set_option(hops3, ec);
-  BOOST_CHECK(!ec);
+    ip::multicast::hops hops3(0);
+    BOOST_CHECK(hops3.value() == 0);
+    sock_v4.set_option(hops3, ec);
+    BOOST_CHECK(!ec);
 
-  ip::multicast::hops hops4;
-  sock.get_option(hops4, ec);
-  BOOST_CHECK(!ec);
-  BOOST_CHECK(hops4.value() == 0);
+    ip::multicast::hops hops4;
+    sock_v4.get_option(hops4, ec);
+    BOOST_CHECK(!ec);
+    BOOST_CHECK(hops4.value() == 0);
+  }
+
+  if (have_v6)
+  {
+    ip::multicast::hops hops1(1);
+    BOOST_CHECK(hops1.value() == 1);
+    sock_v6.set_option(hops1, ec);
+    BOOST_CHECK(!ec);
+
+    ip::multicast::hops hops2;
+    sock_v6.get_option(hops2, ec);
+    BOOST_CHECK(!ec);
+    BOOST_CHECK(hops2.value() == 1);
+
+    ip::multicast::hops hops3(0);
+    BOOST_CHECK(hops3.value() == 0);
+    sock_v6.set_option(hops3, ec);
+    BOOST_CHECK(!ec);
+
+    ip::multicast::hops hops4;
+    sock_v6.get_option(hops4, ec);
+    BOOST_CHECK(!ec);
+    BOOST_CHECK(hops4.value() == 0);
+  }
 
   // enable_loopback class.
 
-  ip::multicast::enable_loopback enable_loopback1(true);
-  BOOST_CHECK(enable_loopback1.value());
-  BOOST_CHECK(static_cast<bool>(enable_loopback1));
-  BOOST_CHECK(!!enable_loopback1);
-  sock.set_option(enable_loopback1, ec);
-  BOOST_CHECK(!ec);
+  if (have_v4)
+  {
+    ip::multicast::enable_loopback enable_loopback1(true);
+    BOOST_CHECK(enable_loopback1.value());
+    BOOST_CHECK(static_cast<bool>(enable_loopback1));
+    BOOST_CHECK(!!enable_loopback1);
+    sock_v4.set_option(enable_loopback1, ec);
+    BOOST_CHECK(!ec);
 
-  ip::multicast::enable_loopback enable_loopback2;
-  sock.get_option(enable_loopback2, ec);
-  BOOST_CHECK(!ec);
-  BOOST_CHECK(enable_loopback2.value());
-  BOOST_CHECK(static_cast<bool>(enable_loopback2));
-  BOOST_CHECK(!!enable_loopback2);
+    ip::multicast::enable_loopback enable_loopback2;
+    sock_v4.get_option(enable_loopback2, ec);
+    BOOST_CHECK(!ec);
+    BOOST_CHECK(enable_loopback2.value());
+    BOOST_CHECK(static_cast<bool>(enable_loopback2));
+    BOOST_CHECK(!!enable_loopback2);
 
-  ip::multicast::enable_loopback enable_loopback3(false);
-  BOOST_CHECK(!enable_loopback3.value());
-  BOOST_CHECK(!static_cast<bool>(enable_loopback3));
-  BOOST_CHECK(!enable_loopback3);
-  sock.set_option(enable_loopback3, ec);
-  BOOST_CHECK(!ec);
+    ip::multicast::enable_loopback enable_loopback3(false);
+    BOOST_CHECK(!enable_loopback3.value());
+    BOOST_CHECK(!static_cast<bool>(enable_loopback3));
+    BOOST_CHECK(!enable_loopback3);
+    sock_v4.set_option(enable_loopback3, ec);
+    BOOST_CHECK(!ec);
 
-  ip::multicast::enable_loopback enable_loopback4;
-  sock.get_option(enable_loopback4, ec);
-  BOOST_CHECK(!ec);
-  BOOST_CHECK(!enable_loopback4.value());
-  BOOST_CHECK(!static_cast<bool>(enable_loopback4));
-  BOOST_CHECK(!enable_loopback4);
+    ip::multicast::enable_loopback enable_loopback4;
+    sock_v4.get_option(enable_loopback4, ec);
+    BOOST_CHECK(!ec);
+    BOOST_CHECK(!enable_loopback4.value());
+    BOOST_CHECK(!static_cast<bool>(enable_loopback4));
+    BOOST_CHECK(!enable_loopback4);
+  }
+
+  if (have_v6)
+  {
+    ip::multicast::enable_loopback enable_loopback1(true);
+    BOOST_CHECK(enable_loopback1.value());
+    BOOST_CHECK(static_cast<bool>(enable_loopback1));
+    BOOST_CHECK(!!enable_loopback1);
+    sock_v6.set_option(enable_loopback1, ec);
+    BOOST_CHECK(!ec);
+
+    ip::multicast::enable_loopback enable_loopback2;
+    sock_v6.get_option(enable_loopback2, ec);
+    BOOST_CHECK(!ec);
+    BOOST_CHECK(enable_loopback2.value());
+    BOOST_CHECK(static_cast<bool>(enable_loopback2));
+    BOOST_CHECK(!!enable_loopback2);
+
+    ip::multicast::enable_loopback enable_loopback3(false);
+    BOOST_CHECK(!enable_loopback3.value());
+    BOOST_CHECK(!static_cast<bool>(enable_loopback3));
+    BOOST_CHECK(!enable_loopback3);
+    sock_v6.set_option(enable_loopback3, ec);
+    BOOST_CHECK(!ec);
+
+    ip::multicast::enable_loopback enable_loopback4;
+    sock_v6.get_option(enable_loopback4, ec);
+    BOOST_CHECK(!ec);
+    BOOST_CHECK(!enable_loopback4.value());
+    BOOST_CHECK(!static_cast<bool>(enable_loopback4));
+    BOOST_CHECK(!enable_loopback4);
+  }
 }
 
 } // namespace ip_multicast_runtime
