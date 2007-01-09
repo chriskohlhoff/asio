720907868ffa2173c25a23300040db8f413c0acb
chris_kohlhoff <chris_kohlhoff>
2007-01-09 04:58:05 +0000

    Add workarounds to compile unit tests with Borland C++.

--- asio/src/tests/unit/io_service.cpp
+++ asio/src/tests/unit/io_service.cpp
@@ -79,6 +79,11 @@ void throw_exception()
   throw 1;
 }
 
+void io_service_run(io_service* ios)
+{
+  ios->run();
+}
+
 void io_service_test()
 {
   io_service ios;
@@ -173,8 +178,8 @@ void io_service_test()
   ios.reset();
   ios.post(boost::bind(start_sleep_increments, &ios, &count));
   ios.post(boost::bind(start_sleep_increments, &ios, &count2));
-  thread thread1(boost::bind(&io_service::run, &ios));
-  thread thread2(boost::bind(&io_service::run, &ios));
+  thread thread1(boost::bind(io_service_run, &ios));
+  thread thread2(boost::bind(io_service_run, &ios));
   thread1.join();
   thread2.join();
 
--- asio/src/tests/unit/strand.cpp
+++ asio/src/tests/unit/strand.cpp
@@ -75,6 +75,11 @@ void throw_exception()
   throw 1;
 }
 
+void io_service_run(io_service* ios)
+{
+  ios->run();
+}
+
 void strand_test()
 {
   io_service ios;
@@ -106,8 +111,8 @@ void strand_test()
   count = 0;
   ios.reset();
   ios.post(boost::bind(start_sleep_increments, &ios, &s, &count));
-  thread thread1(boost::bind(&io_service::run, &ios));
-  thread thread2(boost::bind(&io_service::run, &ios));
+  thread thread1(boost::bind(io_service_run, &ios));
+  thread thread2(boost::bind(io_service_run, &ios));
 
   // Check all events run one after another even though there are two threads.
   deadline_timer timer1(ios, boost::posix_time::seconds(3));
