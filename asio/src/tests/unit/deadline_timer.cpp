7905e7486debdb897b94b28b096ed49a9138e267
chris_kohlhoff <chris_kohlhoff>
2007-05-08 10:47:27 +0000

    Fix test failure caused by incorrect condition check.

--- asio/src/tests/unit/deadline_timer.cpp
+++ asio/src/tests/unit/deadline_timer.cpp
@@ -161,7 +161,7 @@ void deadline_timer_test()
   BOOST_CHECK(count == 0);
   end = now();
   expected_end = start + seconds(2);
-  BOOST_CHECK(end < expected_end);
+  BOOST_CHECK(end <= expected_end);
 
   // Wait on the timer again without cancelling it. This time the asynchronous
   // wait should run to completion and increment the counter.
