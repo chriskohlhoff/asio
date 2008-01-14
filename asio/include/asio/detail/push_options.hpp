7b4f3861530a2b39d7c7b7aed4248e3900d65fbf
chris_kohlhoff <chris_kohlhoff>
2008-01-14 11:51:21 +0000

    Disable noisy and incorrect /Wp64 warnings generated by MSVC.

--- asio/include/asio/detail/push_options.hpp
+++ asio/include/asio/detail/push_options.hpp
@@ -90,6 +90,12 @@
 # pragma warning (disable:4244)
 # pragma warning (disable:4355)
 # pragma warning (disable:4675)
+# if defined(_M_IX86) && defined(_Wp64)
+// The /Wp64 option is broken. If you want to check 64 bit portability, use a
+// 64 bit compiler!
+#  pragma warning (disable:4311)
+#  pragma warning (disable:4312)
+# endif // defined(_M_IX86) && defined(_Wp64)
 # pragma pack (push, 8)
 // Note that if the /Og optimisation flag is enabled with MSVC6, the compiler
 // has a tendency to incorrectly optimise away some calls to member template
