d18a145d47d03f7d836482fd36f9b09d50f80dc1
chris_kohlhoff <chris_kohlhoff>
2008-06-27 05:36:33 +0000

    Add missing copyright notices.

--- asio/src/examples/porthopper/protocol.hpp
+++ asio/src/examples/porthopper/protocol.hpp
@@ -1,3 +1,13 @@
+//
+// protocol.hpp
+// ~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2008 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #ifndef PORTHOPPER_PROTOCOL_HPP
 #define PORTHOPPER_PROTOCOL_HPP
 
--- asio/src/examples/porthopper/server.cpp
+++ asio/src/examples/porthopper/server.cpp
@@ -1,3 +1,13 @@
+//
+// server.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2008 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #include <asio.hpp>
 #include <boost/bind.hpp>
 #include <boost/shared_ptr.hpp>
