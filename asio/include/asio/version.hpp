5d79ded555b28bbdbaa63eb7ee8d3791c1e8c753
chris_kohlhoff <chris_kohlhoff>
2008-10-13 21:52:05 +0000

    asio version 1.3.1 released

--- asio/include/asio/version.hpp
+++ asio/include/asio/version.hpp
@@ -18,6 +18,6 @@
 // ASIO_VERSION % 100 is the sub-minor version
 // ASIO_VERSION / 100 % 1000 is the minor version
 // ASIO_VERSION / 100000 is the major version
-#define ASIO_VERSION 100300 // 1.3.0
+#define ASIO_VERSION 100301 // 1.3.1
 
 #endif // ASIO_VERSION_HPP
