863363a95e0489801f5a33f7a411ba789eae20b1
chris_kohlhoff <chris_kohlhoff>
2008-10-29 12:55:41 +0000

    Fix scoped_ptr usage to work on Sun Studio 11.

--- asio/include/asio/detail/resolver_service.hpp
+++ asio/include/asio/detail/resolver_service.hpp
@@ -329,7 +329,7 @@ private:
   void start_work_thread()
   {
     asio::detail::mutex::scoped_lock lock(mutex_);
-    if (work_thread_ == 0)
+    if (!work_thread_)
     {
       work_thread_.reset(new asio::detail::thread(
             work_io_service_runner(*work_io_service_)));
