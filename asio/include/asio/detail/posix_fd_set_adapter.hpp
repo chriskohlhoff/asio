0ab1b4907ab51682ccfc314531075ef0ebf74e6c
chris_kohlhoff <chris_kohlhoff>
2009-02-01 12:49:37 +0000

    Add include of <cstring> to fix oompile error on Solaris 10.

--- asio/include/asio/detail/posix_fd_set_adapter.hpp
+++ asio/include/asio/detail/posix_fd_set_adapter.hpp
@@ -17,6 +17,10 @@
 
 #include "asio/detail/push_options.hpp"
 
+#include "asio/detail/push_options.hpp"
+#include <cstring>
+#include "asio/detail/pop_options.hpp"
+
 #include "asio/detail/socket_types.hpp"
 
 #if !defined(BOOST_WINDOWS) && !defined(__CYGWIN__)
