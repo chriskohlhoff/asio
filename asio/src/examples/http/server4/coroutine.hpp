f31e080112dde6c143a5b36981e6688033179179
chris_kohlhoff <chris_kohlhoff>
2010-01-13 02:29:51 +0000

    Add coroutine::is_complete() and support for "yield break;".

--- asio/src/examples/http/server4/coroutine.hpp
+++ asio/src/examples/http/server4/coroutine.hpp
@@ -17,6 +17,7 @@ public:
   coroutine() : value_(0) {}
   bool is_child() const { return value_ < 0; }
   bool is_parent() const { return !is_child(); }
+  bool is_complete() const { return value_ == -1; }
 private:
   friend class coroutine_ref;
   int value_;
@@ -25,19 +26,24 @@ private:
 class coroutine_ref
 {
 public:
-  coroutine_ref(coroutine& c) : value_(c.value_) {}
-  coroutine_ref(coroutine* c) : value_(c->value_) {}
+  coroutine_ref(coroutine& c) : value_(c.value_), modified_(false) {}
+  coroutine_ref(coroutine* c) : value_(c->value_), modified_(false) {}
+  ~coroutine_ref() { if (!modified_) value_ = -1; }
   operator int() const { return value_; }
-  int& operator=(int v) { return value_ = v; }
+  int& operator=(int v) { modified_ = true; return value_ = v; }
 private:
   void operator=(const coroutine_ref&);
   int& value_;
+  bool modified_;
 };
 
 #define CORO_REENTER(c) \
   switch (coroutine_ref _coro_value = c) \
     case -1: if (_coro_value) \
     { \
+      goto terminate_coroutine; \
+      terminate_coroutine: \
+      _coro_value = -1; \
       goto bail_out_of_coroutine; \
       bail_out_of_coroutine: \
       break; \
@@ -54,9 +60,12 @@ private:
     else \
       switch (_coro_value ? 0 : 1) \
         for (;;) \
-          case 1: if (_coro_value) \
-            goto bail_out_of_coroutine; \
-          else case 0:
+          case -1: if (_coro_value) \
+            goto terminate_coroutine; \
+          else for (;;) \
+            case 1: if (_coro_value) \
+              goto bail_out_of_coroutine; \
+            else case 0:
 
 #define CORO_FORK \
   for (_coro_value = -__LINE__;; _coro_value = __LINE__) \
