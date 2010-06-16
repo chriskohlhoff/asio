943c6ad4040dc5b7cdf339ade7a160f255e2b9ae
chris_kohlhoff <chris_kohlhoff>
2010-06-16 00:53:34 +0000

    Work-in-progress commit of new timeout example.

---
+++ asio/src/examples/timeouts/async_tcp_client.cpp
@@ -0,0 +1,244 @@
+//
+// async_tcp_client.cpp
+// ~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2010 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include "asio/deadline_timer.hpp"
+#include "asio/io_service.hpp"
+#include "asio/ip/tcp.hpp"
+#include "asio/read_until.hpp"
+#include "asio/streambuf.hpp"
+#include <boost/bind.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+#include <iostream>
+
+using namespace asio;
+using asio::ip::tcp;
+
+//
+// This class consists of two asynchronous "actors":
+//
+//        Protocol Actor                      Timeout Actor
+//        ~~~~~~~~~~~~~~                      ~~~~~~~~~~~~~
+//
+// +---------------+
+// |               |
+// | start_connect |<---+                  +----------------+
+// |               |    |                  |                |     
+// +---------------+    |                  | handle_timeout |<---+
+//         |            |                  |                |    |
+//         |    +----------------+         +----------------+    |
+//         |    |                |                      |        |
+//         +--->| handle_connect |                      +--------+
+//              |                |
+//              +----------------+
+//                      |
+//         +------------+
+//         |
+//         V
+//  +------------+
+//  |            |
+//  | start_read |<----+
+//  |            |     |
+//  +------------+     |
+//         |           |
+//         |    +-------------+
+//         |    |             |
+//         +--->| handle_read |
+//              |             |
+//              +-------------+
+//
+// The protocol actor manages all socket operations, such as connection
+// establishment.
+//
+// The timeout actor is responsible for managing timeouts. When a timeout
+// occurs it will close the socket. This will cause any pending asynchronous
+// operations to complete with the operation_aborted error.
+//
+class client
+{
+public:
+  client(io_service& i)
+    : stopped_(false),
+      timer_(i),
+      socket_(i)
+  {
+  }
+
+  // Called by the user of the client class to initiate the connection process.
+  // The endpoint iterator will have been obtained using a tcp::resolver.
+  void start(tcp::resolver::iterator endpoint_iter)
+  {
+    // Start the protocol actor.
+    start_connect(endpoint_iter);
+
+    // Start the timeout actor. You will note that we're not setting any
+    // particular timeout here. Instead, the protocol actor will update the
+    // timeout prior to each asynchronous operation.
+    timer_.async_wait(boost::bind(&client::handle_timeout, this));
+  }
+
+  // This function terminates the two actors to shut down the connection. It
+  // may be called by the user of the client class, or by the class itself in
+  // response to graceful termination or an unrecoverable error.
+  void stop()
+  {
+    stopped_ = true;
+    socket_.close();
+    timer_.cancel();
+  }
+
+private:
+  void start_connect(tcp::resolver::iterator endpoint_iter)
+  {
+    if (endpoint_iter != tcp::resolver::iterator())
+    {
+      std::cout << "Trying " << endpoint_iter->endpoint() << "...\n";
+
+      // Set a timeout for the connect operation.
+      timer_.expires_from_now(boost::posix_time::seconds(2));
+
+      // Start the asynchronous connect operation.
+      socket_.async_connect(endpoint_iter->endpoint(),
+          boost::bind(&client::handle_connect,
+            this, _1, endpoint_iter));
+    }
+    else
+    {
+      // There are no more endpoints to try. Shut down the client.
+      stop();
+    }
+  }
+
+  void handle_connect(const asio::error_code& ec,
+      tcp::resolver::iterator endpoint_iter)
+  {
+    if (stopped_)
+      return;
+
+    // The async_connect() function automatically opens the socket at the start
+    // of the asynchronous operation. If the socket is closed at this time then
+    // the timeout handler must have run first.
+    if (!socket_.is_open())
+    {
+      std::cout << "Connect timed out\n";
+
+      // Try the next available endpoint.
+      start_connect(++endpoint_iter);
+    }
+
+    // Check if the connect operation failed before the timeout period elapsed.
+    else if (ec)
+    {
+      std::cout << "Connect error: " << ec.message() << "\n";
+
+      // We need to close the socket used in the previous connection attempt
+      // before starting a new one.
+      socket_.close();
+
+      // Try the next available endpoint.
+      start_connect(++endpoint_iter);
+    }
+
+    // Otherwise we have successfully established a connection.
+    else
+    {
+      std::cout << "Connected to " << endpoint_iter->endpoint() << "\n";
+
+      // Start receiving messages from the server.
+      start_read();
+    }
+  }
+
+  void start_read()
+  {
+    // Set a timeout for the read operation.
+    timer_.expires_from_now(boost::posix_time::seconds(30));
+
+    asio::async_read_until(socket_, input_buffer_, '\n',
+        boost::bind(&client::handle_read, this, _1));
+  }
+
+  void handle_read(const asio::error_code& ec)
+  {
+    if (stopped_)
+      return;
+
+    if (!ec)
+    {
+      std::string line;
+      std::istream is(&input_buffer_);
+      std::getline(is, line);
+
+      std::cout << "Received: " << line << "\n";
+
+      start_read();
+    }
+    else
+    {
+      std::cout << "Error: " << ec.message() << "\n";
+
+      stop();
+    }
+  }
+
+  void handle_timeout()
+  {
+    if (stopped_)
+      return;
+
+    // If the timeout actor is being woken up due to timer modification, then
+    // the current expiry time will be in the future. We can use this
+    // information to determine whether to close the socket and so cancel the
+    // pending asynchronous protocol operations.
+    if (timer_.expires_from_now() <= boost::posix_time::seconds(0))
+    {
+      // The timer has truly expired.
+      socket_.close();
+
+      // The timeout actor will enter an indefinite sleep, awaiting further
+      // modifications to the expiry time by the protocol actor.
+      timer_.expires_at(boost::posix_time::pos_infin);
+    }
+
+    // Put the timeout actor back to sleep.
+    timer_.async_wait(boost::bind(&client::handle_timeout, this));
+  }
+
+private:
+  bool stopped_;
+  deadline_timer timer_;
+  tcp::socket socket_;
+  asio::streambuf input_buffer_;
+};
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 3)
+    {
+      std::cerr << "Usage: client <host> <port>\n";
+      return 1;
+    }
+
+    io_service i;
+    tcp::resolver r(i);
+    client c(i);
+
+    c.start(r.resolve(tcp::resolver::query(argv[1], argv[2])));
+
+    i.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
+}
---
+++ asio/src/examples/timeouts/blocking_tcp_client.cpp
@@ -0,0 +1,161 @@
+//
+// blocking_tcp_client.cpp
+// ~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2010 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include "asio/deadline_timer.hpp"
+#include "asio/io_service.hpp"
+#include "asio/ip/tcp.hpp"
+#include "asio/read_until.hpp"
+#include "asio/streambuf.hpp"
+#include "asio/write.hpp"
+#include "asio/thread.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <boost/lambda/bind.hpp>
+#include <boost/lambda/lambda.hpp>
+
+using asio::ip::tcp;
+using boost::lambda::bind;
+using boost::lambda::var;
+using boost::lambda::_1;
+
+class client
+{
+public:
+  client()
+    : socket_(io_service_),
+      timer_(io_service_)
+  {
+    timer_.async_wait(bind(&client::handle_timeout, this));
+  }
+
+  void connect(const std::string& host, const std::string& service,
+      boost::posix_time::time_duration timeout)
+  {
+    timer_.expires_from_now(timeout);
+
+    tcp::resolver::query query(host, service);
+    tcp::resolver::iterator iter = tcp::resolver(io_service_).resolve(query);
+
+    asio::error_code ec;
+
+    for (; iter != tcp::resolver::iterator(); ++iter)
+    {
+      socket_.close();
+
+      ec = asio::error::would_block;
+
+      socket_.async_connect(iter->endpoint(), var(ec) = _1);
+
+      do io_service_.run_one(); while (ec == asio::error::would_block);
+
+      // Determine whether a connection was successfully established.
+      if (!ec && socket_.is_open())
+        return;
+    }
+
+    throw asio::system_error(ec ? ec : asio::error::host_not_found);
+  }
+
+  std::string read_line(boost::posix_time::time_duration timeout)
+  {
+    timer_.expires_from_now(timeout);
+
+    asio::error_code ec = asio::error::would_block;
+
+    // See blocking_udp example for how to use boost::bind rather than lambda in this scenario.
+    asio::async_read_until(socket_, input_buffer_, '\n', var(ec) = _1);
+
+    do io_service_.run_one(); while (ec == asio::error::would_block);
+
+    if (ec)
+      throw asio::system_error(ec);
+
+    std::string line;
+    std::istream is(&input_buffer_);
+    std::getline(is, line);
+    return line;
+  }
+
+  void write_line(const std::string& line,
+      boost::posix_time::time_duration timeout)
+  {
+    std::string data = line + "\n";
+
+    timer_.expires_from_now(timeout);
+
+    asio::error_code ec = asio::error::would_block;
+
+    asio::async_write(socket_, asio::buffer(data), var(ec) = _1);
+
+    do io_service_.run_one(); while (ec == asio::error::would_block);
+
+    if (ec)
+      throw asio::system_error(ec);
+  }
+
+private:
+  void handle_timeout()
+  {
+    if (timer_.expires_from_now() <= boost::posix_time::seconds(0))
+    {
+      socket_.close();
+      timer_.expires_at(boost::posix_time::pos_infin);
+    }
+
+    timer_.async_wait(bind(&client::handle_timeout, this));
+  }
+
+  static void handle_io(const asio::error_code& ec,
+      asio::error_code* out_ec)
+  {
+    *out_ec = ec;
+  }
+
+private:
+  asio::io_service io_service_;
+  tcp::socket socket_;
+  asio::deadline_timer timer_;
+  asio::streambuf input_buffer_;
+};
+
+int response_code(const std::string& line)
+{
+  using namespace std; // For atoi.
+  return atoi(line.c_str());
+}
+
+int more_response(const std::string& line)
+{
+  return line.length() >= 4 && line[3] == '-';
+}
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 2)
+    {
+      std::cerr << "Usage: blocking_tcp <host> <port>\n";
+      return 1;
+    }
+
+    client client;
+    client.connect(argv[1], argv[2], boost::posix_time::seconds(10));
+
+    std::string line = client.read_line(boost::posix_time::seconds(10));
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
+}
