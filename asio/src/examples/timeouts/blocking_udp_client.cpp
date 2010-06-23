e22ef0fa6323dfe527dfcb0aa64be55ec1e6330f
chris_kohlhoff <chris_kohlhoff>
2010-06-23 07:55:14 +0000

    Rename variables, functions and add comments.

--- asio/src/examples/timeouts/blocking_udp_client.cpp
+++ asio/src/examples/timeouts/blocking_udp_client.cpp
@@ -16,46 +16,108 @@
 #include <boost/date_time/posix_time/posix_time_types.hpp>
 #include <iostream>
 
+using asio::deadline_timer;
 using asio::ip::udp;
 
 //----------------------------------------------------------------------
 
+//
+// This class manages socket timeouts by applying the concept of a deadline.
+// Each asynchronous operation is given a deadline by which it must complete.
+// Deadlines are enforced by an "actor" that persists for the lifetime of the
+// client object:
+//
+//  +----------------+
+//  |                |     
+//  | check_deadline |<---+
+//  |                |    |
+//  +----------------+    | async_wait()
+//              |         |
+//              +---------+
+//
+// If the actor determines that the deadline has expired, any outstanding
+// socket operations are cancelled. The socket operations themselves are
+// implemented as transient actors:
+//
+//   +---------------+
+//   |               |
+//   |    receive    |
+//   |               |
+//   +---------------+
+//           |
+//  async_-  |    +----------------+
+// receive() |    |                |
+//           +--->| handle_receive |
+//                |                |
+//                +----------------+
+//
+// The client object runs the io_service to block thread execution until the
+// actor completes.
+//
 class client
 {
 public:
   client(const udp::endpoint& listen_endpoint)
     : socket_(io_service_, listen_endpoint),
-      timer_(io_service_)
+      deadline_(io_service_)
   {
-    timer_.async_wait(boost::bind(&client::handle_timeout, this));
+    // No deadline is required until the first socket operation is started. We
+    // set the deadline to positive infinity so that the actor takes no action
+    // until a specific deadline is set.
+    deadline_.expires_at(boost::posix_time::pos_infin);
+
+    // Start the persistent actor that checks for deadline expiry.
+    check_deadline();
   }
 
   std::size_t receive(const asio::mutable_buffer& buffer,
       boost::posix_time::time_duration timeout, asio::error_code& ec)
   {
-    timer_.expires_from_now(timeout);
+    // Set a deadline for the asynchronous operation.
+    deadline_.expires_from_now(timeout);
 
+    // Set up the variables that receive the result of the asynchronous
+    // operation. The error code is set to would_block to signal that the
+    // operation is incomplete. Asio guarantees that its asynchronous
+    // operations will never fail with would_block, so any other value in
+    // ec indicates completion.
     ec = asio::error::would_block;
     std::size_t length = 0;
 
+    // Start the asynchronous operation itself. The handle_receive function
+    // used as a callback will update the ec and length variables.
     socket_.async_receive(asio::buffer(buffer),
         boost::bind(&client::handle_receive, _1, _2, &ec, &length));
 
+    // Block until the asynchronous operation has completed.
     do io_service_.run_one(); while (ec == asio::error::would_block);
 
     return length;
   }
 
 private:
-  void handle_timeout()
+  void check_deadline()
   {
-    if (timer_.expires_from_now() <= boost::posix_time::seconds(0))
+    // Check whether the deadline has passed. We compare the deadline against
+    // the current time since a new asynchronous operation may have moved the
+    // deadline before this actor had a chance to run.
+    if (deadline_.expires_at() <= deadline_timer::traits_type::now())
     {
+      // The deadline has passed. The outstanding asynchronous operation needs
+      // to be cancelled so that the blocked receive() function will return.
+      //
+      // Please note that cancel() has portability issues on some versions of
+      // Microsoft Windows, and it may be necessary to use close() instead.
+      // Consult the documentation for cancel() for further information.
       socket_.cancel();
-      timer_.expires_at(boost::posix_time::pos_infin);
+
+      // There is no longer an active deadline. The expiry is set to positive
+      // infinity so that the actor takes no action until a new deadline is set.
+      deadline_.expires_at(boost::posix_time::pos_infin);
     }
 
-    timer_.async_wait(boost::bind(&client::handle_timeout, this));
+    // Put the actor back to sleep.
+    deadline_.async_wait(boost::bind(&client::check_deadline, this));
   }
 
   static void handle_receive(
@@ -69,7 +131,7 @@ private:
 private:
   asio::io_service io_service_;
   udp::socket socket_;
-  asio::deadline_timer timer_;
+  deadline_timer deadline_;
 };
 
 //----------------------------------------------------------------------
