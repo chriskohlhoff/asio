db2e65964a99e6ec1c1231f6c88c99ad6e197e34
Christopher Kohlhoff <chris@kohlhoff.com>
2011-03-22 08:05:40 +1100

    Ensure the std::array overloads of asio::buffer() are included in the documentation.

--- asio/include/asio/buffer.hpp
+++ asio/include/asio/buffer.hpp
@@ -976,7 +976,7 @@ inline const_buffers_1 buffer(const boost::array<PodType, N>& data,
         ? data.size() * sizeof(PodType) : max_size_in_bytes));
 }
 
-#if defined(ASIO_HAS_STD_ARRAY)
+#if defined(ASIO_HAS_STD_ARRAY) || defined(GENERATING_DOCUMENTATION)
 
 /// Create a new modifiable buffer that represents the given POD array.
 /**
@@ -1071,7 +1071,7 @@ inline const_buffers_1 buffer(const std::array<PodType, N>& data,
         ? data.size() * sizeof(PodType) : max_size_in_bytes));
 }
 
-#endif // defined(ASIO_HAS_STD_ARRAY)
+#endif // defined(ASIO_HAS_STD_ARRAY) || defined(GENERATING_DOCUMENTATION)
 
 /// Create a new modifiable buffer that represents the given POD vector.
 /**
