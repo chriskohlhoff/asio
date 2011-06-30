4820fd6f0d257a6bb554fcd1f97f170330be0448
Christopher Kohlhoff <chris@kohlhoff.com>
2011-06-30 23:35:32 +1000

    Fix mutex initialisation.

--- asio/include/asio/detail/win_static_mutex.hpp
+++ asio/include/asio/detail/win_static_mutex.hpp
@@ -54,7 +54,11 @@ struct win_static_mutex
   ::CRITICAL_SECTION crit_section_;
 };
 
-#define ASIO_WIN_STATIC_MUTEX_INIT { false, { 0, 0, 0, 0, 0, 0 } }
+#if defined(UNDER_CE)
+# define ASIO_WIN_STATIC_MUTEX_INIT { false, { 0, 0, 0, 0, 0 } }
+#else // defined(UNDER_CE)
+# define ASIO_WIN_STATIC_MUTEX_INIT { false, { 0, 0, 0, 0, 0, 0 } }
+#endif // defined(UNDER_CE)
 
 } // namespace detail
 } // namespace asio
