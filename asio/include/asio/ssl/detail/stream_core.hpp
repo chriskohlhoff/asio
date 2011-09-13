f6c7cb11c4233f81542c421f4f19c2eecfedc8f4
Christopher Kohlhoff <chris@kohlhoff.com>
2011-09-14 09:51:15 +1000

    Change the SSL buffer sizes so that they're large enough to hold a complete TLS record.

--- asio/include/asio/ssl/detail/stream_core.hpp
+++ asio/include/asio/ssl/detail/stream_core.hpp
@@ -33,13 +33,17 @@ namespace detail {
 
 struct stream_core
 {
+  // According to the OpenSSL documentation, this is the buffer size that is is
+  // sufficient to hold the largest possible TLS record.
+  enum { max_tls_record_size = 17 * 1024 };
+
   stream_core(SSL_CTX* context, asio::io_service& io_service)
     : engine_(context),
       pending_read_(io_service),
       pending_write_(io_service),
-      output_buffer_space_(16384),
+      output_buffer_space_(max_tls_record_size),
       output_buffer_(asio::buffer(output_buffer_space_)),
-      input_buffer_space_(16384),
+      input_buffer_space_(max_tls_record_size),
       input_buffer_(asio::buffer(input_buffer_space_))
   {
     pending_read_.expires_at(boost::posix_time::neg_infin);
