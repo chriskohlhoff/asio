fc52e68e8458c652fd93095f14b30780fb6880dd
Christopher Kohlhoff <chris@kohlhoff.com>
2013-05-07 07:40:32 +1000

    Tidy up example and convert it to use only C++03 features.

--- asio/src/examples/spawn/echo_server.cpp
+++ asio/src/examples/spawn/echo_server.cpp
@@ -1,30 +1,48 @@
+//
+// echo_server.cpp
+// ~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2012 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <asio/deadline_timer.hpp>
 #include <asio/io_service.hpp>
 #include <asio/ip/tcp.hpp>
 #include <asio/spawn.hpp>
-#include <asio/steady_timer.hpp>
 #include <asio/write.hpp>
-#include <memory>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <iostream>
 
 using asio::ip::tcp;
 
-class session : public std::enable_shared_from_this<session>
+class session : public boost::enable_shared_from_this<session>
 {
 public:
-  explicit session(tcp::socket socket)
-    : strand_(socket.get_io_service()),
-      socket_(std::move(socket)),
-      timer_(strand_.get_io_service())
+  explicit session(asio::io_service& io_service)
+    : strand_(io_service),
+      socket_(io_service),
+      timer_(io_service)
   {
+  }
+
+  tcp::socket& socket()
+  {
+    return socket_;
   }
 
   void go()
   {
     asio::spawn(strand_,
-        std::bind(&session::echo,
-          shared_from_this(), std::placeholders::_1));
+        boost::bind(&session::echo,
+          shared_from_this(), _1));
     asio::spawn(strand_,
-        std::bind(&session::timeout,
-          shared_from_this(), std::placeholders::_1));
+        boost::bind(&session::timeout,
+          shared_from_this(), _1));
   }
 
 private:
@@ -35,7 +53,7 @@ private:
       char data[128];
       for (;;)
       {
-        timer_.expires_from_now(std::chrono::seconds(10));
+        timer_.expires_from_now(boost::posix_time::seconds(10));
         std::size_t n = socket_.async_read_some(asio::buffer(data), yield);
         asio::async_write(socket_, asio::buffer(data, n), yield);
       }
@@ -51,34 +69,54 @@ private:
   {
     while (socket_.is_open())
     {
-      std::error_code ignored_ec;
+      asio::error_code ignored_ec;
       timer_.async_wait(yield(ignored_ec));
-      if (timer_.expires_from_now() <= std::chrono::seconds(0))
+      if (timer_.expires_from_now() <= boost::posix_time::seconds(0))
         socket_.close();
     }
   }
 
   asio::io_service::strand strand_;
   tcp::socket socket_;
-  asio::steady_timer timer_;
+  asio::deadline_timer timer_;
 };
 
-int main()
+void do_accept(asio::io_service& io_service,
+    unsigned short port, asio::yield_context yield)
 {
-  asio::io_service io_service;
+  tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), port));
 
-  asio::spawn(io_service,
-      [&](asio::yield_context yield)
-      {
-        tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 55555));
-        for (;;)
-        {
-          std::error_code ec;
-          tcp::socket socket(io_service);
-          acceptor.async_accept(socket, yield(ec));
-          if (!ec) std::make_shared<session>(std::move(socket))->go();
-        }
-      });
+  for (;;)
+  {
+    asio::error_code ec;
+    boost::shared_ptr<session> new_session(new session(io_service));
+    acceptor.async_accept(new_session->socket(), yield(ec));
+    if (!ec) new_session->go();
+  }
+}
 
-  io_service.run();
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 2)
+    {
+      std::cerr << "Usage: echo_server <port>\n";
+      return 1;
+    }
+
+    asio::io_service io_service;
+
+    asio::spawn(io_service,
+        boost::bind(do_accept,
+          boost::ref(io_service), atoi(argv[1]), _1));
+
+    io_service.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
 }
