46ae8fab41404e8cd784604783c790b090e670f4
Christopher Kohlhoff <chris@kohlhoff.com>
2014-05-03 12:15:14 +1000

    Remove unused data member.

--- asio/src/examples/cpp03/echo/async_udp_echo_server.cpp
+++ asio/src/examples/cpp03/echo/async_udp_echo_server.cpp
@@ -19,8 +19,7 @@ class server
 {
 public:
   server(asio::io_service& io_service, short port)
-    : io_service_(io_service),
-      socket_(io_service, udp::endpoint(udp::v4(), port))
+    : socket_(io_service, udp::endpoint(udp::v4(), port))
   {
     socket_.async_receive_from(
         asio::buffer(data_, max_length), sender_endpoint_,
@@ -61,7 +60,6 @@ public:
   }
 
 private:
-  asio::io_service& io_service_;
   udp::socket socket_;
   udp::endpoint sender_endpoint_;
   enum { max_length = 1024 };
