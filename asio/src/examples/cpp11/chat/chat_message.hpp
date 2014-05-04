2f6c67c266dc25544240491973ed033ecfa2fef9
Christopher Kohlhoff <chris@kohlhoff.com>
2014-05-04 10:05:28 +1000

    Fix another sprintf warning in example.

--- asio/src/examples/cpp11/chat/chat_message.hpp
+++ asio/src/examples/cpp11/chat/chat_message.hpp
@@ -79,7 +79,7 @@ public:
   void encode_header()
   {
     char header[header_length + 1] = "";
-    std::sprintf(header, "%4d", body_length_);
+    std::sprintf(header, "%4d", static_cast<int>(body_length_));
     std::memcpy(data_, header, header_length);
   }
 
