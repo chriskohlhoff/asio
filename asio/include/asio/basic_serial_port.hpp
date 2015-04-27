f1906058e1a2764043bba12517aaed7e4ee75d2c
Christopher Kohlhoff <chris@kohlhoff.com>
2015-04-28 09:47:15 +1000

    Fix typo in comment.

--- asio/include/asio/basic_serial_port.hpp
+++ asio/include/asio/basic_serial_port.hpp
@@ -444,7 +444,7 @@ public:
    *
    * @param option The option value to be obtained from the serial port.
    *
-   * @param ec Set to indicate what error occured, if any.
+   * @param ec Set to indicate what error occurred, if any.
    *
    * @sa GettableSerialPortOption @n
    * asio::serial_port_base::baud_rate @n
