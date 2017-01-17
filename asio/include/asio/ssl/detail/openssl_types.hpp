f3f324cbe9d805cb310f8eb3ec00a2060a433177
Christopher Kohlhoff <chris@kohlhoff.com>
2017-01-18 08:00:22 +1100

    Include socket_types.hpp before OpenSSL headers.

--- asio/include/asio/ssl/detail/openssl_types.hpp
+++ asio/include/asio/ssl/detail/openssl_types.hpp
@@ -16,6 +16,7 @@
 #endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
 
 #include "asio/detail/config.hpp"
+#include "asio/detail/socket_types.hpp"
 #include <openssl/conf.h>
 #include <openssl/ssl.h>
 #if !defined(OPENSSL_NO_ENGINE)
@@ -25,6 +26,5 @@
 #include <openssl/err.h>
 #include <openssl/rsa.h>
 #include <openssl/x509v3.h>
-#include "asio/detail/socket_types.hpp"
 
 #endif // ASIO_SSL_DETAIL_OPENSSL_TYPES_HPP
