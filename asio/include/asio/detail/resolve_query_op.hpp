b8a34a1dc0285259408365695119dd29747fcd34
Christopher Kohlhoff <chris@kohlhoff.com>
2017-07-15 16:27:46 +1000

    Fix work counting issue in asynchronous resolver implementation.

--- asio/include/asio/detail/resolve_query_op.hpp
+++ asio/include/asio/detail/resolve_query_op.hpp
@@ -67,7 +67,6 @@ public:
     // Take ownership of the operation object.
     resolve_query_op* o(static_cast<resolve_query_op*>(base));
     ptr p = { asio::detail::addressof(o->handler_), o, o };
-    handler_work<Handler> w(o->handler_);
 
     if (owner && owner != &o->io_context_impl_)
     {
@@ -87,6 +86,9 @@ public:
     {
       // The operation has been returned to the main io_context. The completion
       // handler is ready to be delivered.
+
+      // Take ownership of the operation's outstanding work.
+      handler_work<Handler> w(o->handler_);
 
       ASIO_HANDLER_COMPLETION((*o));
 
