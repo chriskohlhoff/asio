ccbf1168c71ca46e84099cd1e7cb5a04515229e4
Christopher Kohlhoff <chris@kohlhoff.com>
2018-04-20 10:40:24 +1000

    Add headers corresponding to unified executors proposal.

---
+++ asio/include/asio/ts/execution.hpp
@@ -0,0 +1,20 @@
+//
+// ts/execution.hpp
+// ~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2018 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef ASIO_TS_EXECUTION_HPP
+#define ASIO_TS_EXECUTION_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include "asio/execution.hpp"
+
+#endif // ASIO_TS_EXECUTION_HPP
