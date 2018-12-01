019449fb0f0cae6c54c4e9b76feee6de72f26187
Christopher Kohlhoff <chris@kohlhoff.com>
2018-12-02 08:22:09 +1100

    Use new form of async_accept where socket is moved into the completion handler.

--- asio/src/examples/cpp11/invocation/prioritised_handlers.cpp
+++ asio/src/examples/cpp11/invocation/prioritised_handlers.cpp
@@ -142,7 +142,8 @@ private:
 
 //----------------------------------------------------------------------
 
-void high_priority_handler(const asio::error_code& /*ec*/)
+void high_priority_handler(const asio::error_code& /*ec*/,
+    tcp::socket /*socket*/)
 {
   std::cout << "High priority handler\n";
 }
@@ -178,8 +179,7 @@ int main()
   tcp::endpoint endpoint(asio::ip::address_v4::loopback(), 0);
   tcp::acceptor acceptor(io_context, endpoint);
   tcp::socket server_socket(io_context);
-  acceptor.async_accept(server_socket,
-      pri_queue.wrap(100, high_priority_handler));
+  acceptor.async_accept(pri_queue.wrap(100, high_priority_handler));
   tcp::socket client_socket(io_context);
   client_socket.connect(acceptor.local_endpoint());
 
