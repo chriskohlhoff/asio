e7b397142ae11545ea08fcf04db3008f588b4ce7
Christopher Kohlhoff <chris@kohlhoff.com>
2019-02-21 14:48:59 +1100

    Update windows::overlapped_ptr to support custom I/O executors.

--- asio/src/tests/unit/windows/overlapped_ptr.cpp
+++ asio/src/tests/unit/windows/overlapped_ptr.cpp
@@ -16,6 +16,7 @@
 // Test that header file is self-contained.
 #include "asio/windows/overlapped_ptr.hpp"
 
+#include "asio/executor.hpp"
 #include "asio/io_context.hpp"
 #include "../unit_test.hpp"
 
@@ -49,6 +50,7 @@ void test()
   try
   {
     io_context ioc;
+    asio::executor ex(ioc.get_executor());
 
     // basic_overlapped_ptr constructors.
 
@@ -57,6 +59,11 @@ void test()
     win::overlapped_ptr ptr2(ioc, &overlapped_handler_1);
     win::overlapped_ptr ptr3(ioc, overlapped_handler_2());
 
+    win::overlapped_ptr ptr4(ioc.get_executor(), &overlapped_handler_1);
+    win::overlapped_ptr ptr5(ioc.get_executor(), overlapped_handler_2());
+    win::overlapped_ptr ptr6(ex, &overlapped_handler_1);
+    win::overlapped_ptr ptr7(ex, overlapped_handler_2());
+
     // overlapped_ptr functions.
 
     ptr1.reset();
@@ -64,10 +71,15 @@ void test()
     ptr2.reset(ioc, &overlapped_handler_1);
     ptr3.reset(ioc, overlapped_handler_2());
 
+    ptr2.reset(ioc.get_executor(), &overlapped_handler_1);
+    ptr3.reset(ioc.get_executor(), overlapped_handler_2());
+    ptr2.reset(ex, &overlapped_handler_1);
+    ptr3.reset(ex, overlapped_handler_2());
+
     OVERLAPPED* ov1 = ptr1.get();
     (void)ov1;
 
-    const win::overlapped_ptr& ptr4(ptr1);
+    const win::overlapped_ptr& ptr8(ptr1);
     const OVERLAPPED* ov2 = ptr4.get();
     (void)ov2;
 
