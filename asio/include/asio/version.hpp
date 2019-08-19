62aea6b2073a104dbaaec80be7c04f4770acb615
Christopher Kohlhoff <chris@kohlhoff.com>
2019-08-19 20:45:13 +1000

    asio version 1.14.1 released

--- asio/include/asio/version.hpp
+++ asio/include/asio/version.hpp
@@ -18,6 +18,6 @@
 // ASIO_VERSION % 100 is the sub-minor version
 // ASIO_VERSION / 100 % 1000 is the minor version
 // ASIO_VERSION / 100000 is the major version
-#define ASIO_VERSION 101400 // 1.14.0
+#define ASIO_VERSION 101401 // 1.14.1
 
 #endif // ASIO_VERSION_HPP
