658614dca67896c0cec896efcc089f5028de1170
Christopher Kohlhoff <chris@kohlhoff.com>
2020-05-03 19:23:15 +1000

    Add move constructor to ssl::stream<>.

--- asio/src/tests/unit/ssl/stream.cpp
+++ asio/src/tests/unit/ssl/stream.cpp
@@ -75,6 +75,12 @@ void test()
     ip::tcp::socket socket1(ioc, ip::tcp::v4());
     ssl::stream<ip::tcp::socket&> stream2(socket1, context);
 
+#if defined(ASIO_HAS_MOVE)
+    ssl::stream<ip::tcp::socket> stream3
+      = ssl::stream<ip::tcp::socket>(ioc, context);
+    ssl::stream<ip::tcp::socket> stream4(std::move(stream3));
+#endif // defined(ASIO_HAS_MOVE)
+
     // basic_io_object functions.
 
     ssl::stream<ip::tcp::socket>::executor_type ex = stream1.get_executor();
@@ -89,9 +95,9 @@ void test()
       = stream1.lowest_layer();
     (void)lowest_layer;
 
-    const ssl::stream<ip::tcp::socket>& stream3 = stream1;
+    const ssl::stream<ip::tcp::socket>& stream5 = stream1;
     const ssl::stream<ip::tcp::socket>::lowest_layer_type& lowest_layer2
-      = stream3.lowest_layer();
+      = stream5.lowest_layer();
     (void)lowest_layer2;
 
     stream1.set_verify_mode(ssl::verify_none);
