4a899105c6752999c43513b68c111b720a658cc2
Christopher Kohlhoff <chris@kohlhoff.com>
2020-05-18 15:04:06 +1000

    Add execution::blocking_adaptation property.

--- asio/include/asio.hpp
+++ asio/include/asio.hpp
@@ -54,6 +54,7 @@
 #include "asio/error.hpp"
 #include "asio/error_code.hpp"
 #include "asio/execution/blocking.hpp"
+#include "asio/execution/blocking_adaptation.hpp"
 #include "asio/execution/execute.hpp"
 #include "asio/execution/executor.hpp"
 #include "asio/execution/invocable_archetype.hpp"
