a451ec4ad0969cac7376fe56284a1aff73c8c9d2
Christopher Kohlhoff <chris@kohlhoff.com>
2020-06-29 16:18:32 +1000

    Add execution::sender_to concept and execution::is_sender_to trait.

--- asio/include/asio/execution/sender.hpp
+++ asio/include/asio/execution/sender.hpp
@@ -17,6 +17,7 @@
 
 #include "asio/detail/config.hpp"
 #include "asio/detail/type_traits.hpp"
+#include "asio/execution/receiver.hpp"
 
 #include "asio/detail/push_options.hpp"
 
@@ -120,9 +121,57 @@ ASIO_CONCEPT sender = is_sender<T>::value;
 
 #endif // defined(ASIO_HAS_CONCEPTS)
 
+template <typename S, typename R>
+struct can_connect;
+
+/// The is_sender_to trait detects whether a type T satisfies the
+/// execution::sender_to concept for some receiver.
+/**
+ * Class template @c is_sender_to is a type trait that is derived from @c
+ * true_type if the type @c T meets the concept definition for a sender
+ * for some receiver type R, otherwise @c false.
+ */
+template <typename T, typename R>
+struct is_sender_to :
+#if defined(GENERATING_DOCUMENTATION)
+  integral_constant<bool, automatically_determined>
+#else // defined(GENERATING_DOCUMENTATION)
+  integral_constant<bool,
+    is_sender<T>::value
+      && is_receiver<R>::value
+      && can_connect<T, R>::value
+  >
+#endif // defined(GENERATING_DOCUMENTATION)
+{
+};
+
+#if defined(ASIO_HAS_VARIABLE_TEMPLATES)
+
+template <typename T, typename R>
+ASIO_CONSTEXPR const bool is_sender_to_v =
+  is_sender_to<T, R>::value;
+
+#endif // defined(ASIO_HAS_VARIABLE_TEMPLATES)
+
+#if defined(ASIO_HAS_CONCEPTS)
+
+template <typename T, typename R>
+ASIO_CONCEPT sender_to = is_sender_to<T, R>::value;
+
+#define ASIO_EXECUTION_SENDER_TO(r) \
+  ::asio::execution::sender_to<r>
+
+#else // defined(ASIO_HAS_CONCEPTS)
+
+#define ASIO_EXECUTION_SENDER_TO(r) typename
+
+#endif // defined(ASIO_HAS_CONCEPTS)
+
 } // namespace execution
 } // namespace asio
 
 #include "asio/detail/pop_options.hpp"
+
+#include "asio/execution/connect.hpp"
 
 #endif // ASIO_EXECUTION_SENDER_HPP
