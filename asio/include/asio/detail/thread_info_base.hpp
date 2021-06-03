dfbc39f5fd564be30ad987238c7d792778882603
Christopher Kohlhoff <chris@kohlhoff.com>
2021-06-03 11:48:08 +1000

    Increase number of cached memory slots for default recycling allocator.
    The number of per-thread slots may be configured at compile time by
    defining ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE to a non-negative integer.
    If not defined, the default recycling allocator uses two slots.

--- asio/include/asio/detail/thread_info_base.hpp
+++ asio/include/asio/detail/thread_info_base.hpp
@@ -39,18 +39,36 @@ class thread_info_base
 public:
   struct default_tag
   {
-    enum { mem_index = 0 };
+    enum
+    {
+      begin_mem_index = 0,
+#ifdef ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
+      end_mem_index = ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
+#else // ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
+      end_mem_index = 2
+#endif // ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
+    };
   };
 
   struct awaitable_frame_tag
   {
-    enum { mem_index = 1 };
+    enum
+    {
+      begin_mem_index = default_tag::end_mem_index,
+      end_mem_index = begin_mem_index + 1
+    };
   };
 
   struct executor_function_tag
   {
-    enum { mem_index = 2 };
+    enum
+    {
+      begin_mem_index = awaitable_frame_tag::end_mem_index,
+      end_mem_index = begin_mem_index + 1
+    };
   };
+
+  enum { max_mem_index = executor_function_tag::end_mem_index };
 
   thread_info_base()
 #if defined(ASIO_HAS_STD_EXCEPTION_PTR) \
@@ -93,20 +111,36 @@ public:
   {
     std::size_t chunks = (size + chunk_size - 1) / chunk_size;
 
-    if (this_thread && this_thread->reusable_memory_[Purpose::mem_index])
+    if (this_thread)
     {
-      void* const pointer = this_thread->reusable_memory_[Purpose::mem_index];
-      this_thread->reusable_memory_[Purpose::mem_index] = 0;
-
-      unsigned char* const mem = static_cast<unsigned char*>(pointer);
-      if (static_cast<std::size_t>(mem[0]) >= chunks
-          && reinterpret_cast<std::size_t>(pointer) % align == 0)
+      for (int mem_index = Purpose::begin_mem_index;
+          mem_index < Purpose::end_mem_index; ++mem_index)
       {
-        mem[size] = mem[0];
-        return pointer;
+        if (this_thread->reusable_memory_[mem_index])
+        {
+          void* const pointer = this_thread->reusable_memory_[mem_index];
+          unsigned char* const mem = static_cast<unsigned char*>(pointer);
+          if (static_cast<std::size_t>(mem[0]) >= chunks
+              && reinterpret_cast<std::size_t>(pointer) % align == 0)
+          {
+            this_thread->reusable_memory_[mem_index] = 0;
+            mem[size] = mem[0];
+            return pointer;
+          }
+        }
       }
 
-      aligned_delete(pointer);
+      for (int mem_index = Purpose::begin_mem_index;
+          mem_index < Purpose::end_mem_index; ++mem_index)
+      {
+        if (this_thread->reusable_memory_[mem_index])
+        {
+          void* const pointer = this_thread->reusable_memory_[mem_index];
+          this_thread->reusable_memory_[mem_index] = 0;
+          aligned_delete(pointer);
+          break;
+        }
+      }
     }
 
     void* const pointer = aligned_new(align, chunks * chunk_size + 1);
@@ -121,12 +155,19 @@ public:
   {
     if (size <= chunk_size * UCHAR_MAX)
     {
-      if (this_thread && this_thread->reusable_memory_[Purpose::mem_index] == 0)
+      if (this_thread)
       {
-        unsigned char* const mem = static_cast<unsigned char*>(pointer);
-        mem[0] = mem[size];
-        this_thread->reusable_memory_[Purpose::mem_index] = pointer;
-        return;
+        for (int mem_index = Purpose::begin_mem_index;
+            mem_index < Purpose::end_mem_index; ++mem_index)
+        {
+          if (this_thread->reusable_memory_[mem_index] == 0)
+          {
+            unsigned char* const mem = static_cast<unsigned char*>(pointer);
+            mem[0] = mem[size];
+            this_thread->reusable_memory_[mem_index] = pointer;
+            return;
+          }
+        }
       }
     }
 
@@ -174,7 +215,6 @@ public:
 
 private:
   enum { chunk_size = 4 };
-  enum { max_mem_index = 3 };
   void* reusable_memory_[max_mem_index];
 
 #if defined(ASIO_HAS_STD_EXCEPTION_PTR) \
