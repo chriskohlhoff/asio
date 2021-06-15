1f0a86425e152ea03dbbd341377b3f35a2aec585
Stephen Whittle <swhittle@crypticwarrior.net>
2021-06-15 16:25:26 +1000

    Add some additional configuration defines to force use of functions that are supported on nx

--- asio/include/asio/detail/select_interrupter.hpp
+++ asio/include/asio/detail/select_interrupter.hpp
@@ -19,7 +19,7 @@
 
 #if !defined(ASIO_WINDOWS_RUNTIME)
 
-#if defined(ASIO_WINDOWS) || defined(__CYGWIN__) || defined(__SYMBIAN32__)
+#if defined(ASIO_WINDOWS) || defined(__CYGWIN__) || defined(__SYMBIAN32__) || defined(ASIO_USE_SOCKET_SELECT)
 # include "asio/detail/socket_select_interrupter.hpp"
 #elif defined(ASIO_HAS_EVENTFD)
 # include "asio/detail/eventfd_select_interrupter.hpp"
@@ -30,8 +30,8 @@
 namespace asio {
 namespace detail {
 
-#if defined(ASIO_WINDOWS) || defined(__CYGWIN__) || defined(__SYMBIAN32__)
-typedef socket_select_interrupter select_interrupter;
+#if defined(ASIO_WINDOWS) || defined(__CYGWIN__) || defined(__SYMBIAN32__) || defined(ASIO_USE_SOCKET_SELECT)
+	typedef socket_select_interrupter select_interrupter;
 #elif defined(ASIO_HAS_EVENTFD)
 typedef eventfd_select_interrupter select_interrupter;
 #else
