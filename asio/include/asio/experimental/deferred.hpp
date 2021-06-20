05c3ad0e3e0a586b56167b60e29965b98657ac34
Christopher Kohlhoff <chris@kohlhoff.com>
2021-06-20 18:44:12 +1000

    Rename lazy to deferred.

--- asio/include/asio/experimental/deferred.hpp
+++ asio/include/asio/experimental/deferred.hpp
@@ -1,15 +1,15 @@
 //
-// experimental/lazy.hpp
-// ~~~~~~~~~~~~~~~~~~~~~
+// experimental/deferred.hpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// Copyright (c) 2003-2020 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Copyright (c) 2003-2021 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#ifndef ASIO_EXPERIMENTAL_LAZY_HPP
-#define ASIO_EXPERIMENTAL_LAZY_HPP
+#ifndef ASIO_EXPERIMENTAL_DEFERRED_HPP
+#define ASIO_EXPERIMENTAL_DEFERRED_HPP
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1200)
 # pragma once
@@ -23,7 +23,7 @@ namespace asio {
 namespace experimental {
 namespace detail {
 
-struct lazy_empty_branch
+struct deferred_empty_branch
 {
   template <typename... Args>
   void operator()(ASIO_MOVE_ARG(Args)...) ASIO_RVALUE_REF_QUAL
@@ -33,39 +33,39 @@ struct lazy_empty_branch
 
 } // namespace detail
 
-template <typename Impl> class lazy_operation;
-template <typename OnTrue, typename OnFalse> class lazy_condition;
+template <typename Impl> class deferred_operation;
+template <typename OnTrue, typename OnFalse> class deferred_condition;
 
 /// Class used to specify that an asynchronous operation should return a
 /// function object to lazily launch the operation.
 /**
- * The lazy_t class is used to indicate that an asynchronous operation should
- * return a function object which is itself an initiation function. A lazy_t
+ * The deferred_t class is used to indicate that an asynchronous operation should
+ * return a function object which is itself an initiation function. A deferred_t
  * object may be passed as a completion token to an asynchronous operation,
- * typically using the special value @c asio::lazy. For example:
+ * typically using the special value @c asio::deferred. For example:
  *
  * @code auto my_sender
  *   = my_socket.async_read_some(my_buffer,
- *       asio::experimental::lazy); @endcode
+ *       asio::experimental::deferred); @endcode
  *
  * The initiating function (async_read_some in the above example) returns a
  * function object that will lazily initiate the operation.
  */
-class lazy_t
+class deferred_t
 {
 public:
   /// Default constructor.
-  ASIO_CONSTEXPR lazy_t()
+  ASIO_CONSTEXPR deferred_t()
   {
   }
 
-  /// Adapts an executor to add the @c lazy_t completion token as the
+  /// Adapts an executor to add the @c deferred_t completion token as the
   /// default.
   template <typename InnerExecutor>
   struct executor_with_default : InnerExecutor
   {
-    /// Specify @c lazy_t as the default completion token type.
-    typedef lazy_t default_completion_token_type;
+    /// Specify @c deferred_t as the default completion token type.
+    typedef deferred_t default_completion_token_type;
 
     /// Construct the adapted executor from the inner executor type.
     template <typename InnerExecutor1>
@@ -82,7 +82,7 @@ public:
     }
   };
 
-  /// Type alias to adapt an I/O object to use @c lazy_t as its
+  /// Type alias to adapt an I/O object to use @c deferred_t as its
   /// default completion token type.
 #if defined(ASIO_HAS_ALIAS_TEMPLATES) \
   || defined(GENERATING_DOCUMENTATION)
@@ -92,7 +92,7 @@ public:
 #endif // defined(ASIO_HAS_ALIAS_TEMPLATES)
        //   || defined(GENERATING_DOCUMENTATION)
 
-  /// Function helper to adapt an I/O object to use @c lazy_t as its
+  /// Function helper to adapt an I/O object to use @c deferred_t as its
   /// default completion token type.
   template <typename T>
   static typename decay<T>::type::template rebind_executor<
@@ -105,34 +105,34 @@ public:
       >::other(ASIO_MOVE_CAST(T)(object));
   }
 
-  /// Creates a conditional object for branching lazy operations.
-  static lazy_condition<detail::lazy_empty_branch,
-    detail::lazy_empty_branch> when(bool b);
+  /// Creates a conditional object for branching deferred operations.
+  static deferred_condition<detail::deferred_empty_branch,
+    detail::deferred_empty_branch> when(bool b);
 
-  /// Returns a lazy operation that returns the provided values.
+  /// Returns a deferred operation that returns the provided values.
   template <typename... Args>
   static auto values(Args... args);
 };
 
-/// Class used to encapsulate the result of a lazy operation.
+/// Class used to encapsulate the result of a deferred operation.
 template <typename Impl>
-class lazy_operation : public Impl
+class deferred_operation : public Impl
 {
 public:
   template <typename I>
-  explicit lazy_operation(ASIO_MOVE_ARG(I) impl)
+  explicit deferred_operation(ASIO_MOVE_ARG(I) impl)
     : Impl(ASIO_MOVE_CAST(I)(impl))
   {
   }
 };
 
-/// Class used to make conditional lazy branches.
+/// Class used to make conditional deferred branches.
 template <typename OnTrue, typename OnFalse>
-class lazy_condition
+class deferred_condition
 {
 public:
   template <typename T, typename F>
-  explicit lazy_condition(bool b, ASIO_MOVE_ARG(T) on_true,
+  explicit deferred_condition(bool b, ASIO_MOVE_ARG(T) on_true,
       ASIO_MOVE_ARG(F) on_false)
     : on_true_(ASIO_MOVE_CAST(T)(on_true)),
       on_false_(ASIO_MOVE_CAST(F)(on_false)),
@@ -156,39 +156,39 @@ public:
   }
 
   template <typename Impl>
-  lazy_operation<lazy_condition<Impl, OnFalse>> then(
-      lazy_operation<Impl> op,
+  deferred_operation<deferred_condition<Impl, OnFalse>> then(
+      deferred_operation<Impl> op,
       typename constraint<
         is_same<
           typename conditional<true, OnTrue, Impl>::type,
-          detail::lazy_empty_branch
+          detail::deferred_empty_branch
         >::value
       >::type* = 0) ASIO_RVALUE_REF_QUAL
   {
-    return lazy_operation<lazy_condition<Impl, OnFalse>>(
-        lazy_condition<Impl, OnFalse>(bool_,
+    return deferred_operation<deferred_condition<Impl, OnFalse>>(
+        deferred_condition<Impl, OnFalse>(bool_,
           ASIO_MOVE_CAST(Impl)(op),
           ASIO_MOVE_CAST(OnFalse)(on_false_)));
   }
 
   template <typename Impl>
-  lazy_operation<lazy_condition<OnTrue, Impl>> otherwise(
-      lazy_operation<Impl> op,
+  deferred_operation<deferred_condition<OnTrue, Impl>> otherwise(
+      deferred_operation<Impl> op,
       typename constraint<
         !is_same<
           typename conditional<true, OnTrue, Impl>::type,
-          detail::lazy_empty_branch
+          detail::deferred_empty_branch
         >::value
       >::type* = 0,
       typename constraint<
         is_same<
           typename conditional<true, OnFalse, Impl>::type,
-          detail::lazy_empty_branch
+          detail::deferred_empty_branch
         >::value
       >::type* = 0) ASIO_RVALUE_REF_QUAL
   {
-    return lazy_operation<lazy_condition<OnTrue, Impl>>(
-        lazy_condition<OnTrue, Impl>(bool_,
+    return deferred_operation<deferred_condition<OnTrue, Impl>>(
+        deferred_condition<OnTrue, Impl>(bool_,
           ASIO_MOVE_CAST(OnTrue)(on_true_),
           ASIO_MOVE_CAST(Impl)(op)));
   }
@@ -199,53 +199,53 @@ private:
   bool bool_;
 };
 
-inline lazy_condition<detail::lazy_empty_branch, detail::lazy_empty_branch>
-lazy_t::when(bool b)
+inline deferred_condition<detail::deferred_empty_branch, detail::deferred_empty_branch>
+deferred_t::when(bool b)
 {
-  return lazy_condition<detail::lazy_empty_branch, detail::lazy_empty_branch>(
-      b, detail::lazy_empty_branch(), detail::lazy_empty_branch());
+  return deferred_condition<detail::deferred_empty_branch, detail::deferred_empty_branch>(
+      b, detail::deferred_empty_branch(), detail::deferred_empty_branch());
 }
 
 template <typename... Args>
-auto lazy_t::values(Args... args)
+auto deferred_t::values(Args... args)
 {
-  return asio::async_initiate<const lazy_t&, void(Args...)>(
+  return asio::async_initiate<const deferred_t&, void(Args...)>(
       [](auto handler, auto... args)
       {
         handler(ASIO_MOVE_CAST(Args)(args)...);
-      }, lazy_t(), ASIO_MOVE_CAST(Args)(args)...);
+      }, deferred_t(), ASIO_MOVE_CAST(Args)(args)...);
 }
 
-/// Pipe operator used to chain lazy operations.
+/// Pipe operator used to chain deferred operations.
 template <typename Impl, typename CompletionToken>
-auto operator|(lazy_operation<Impl> head,
+auto operator|(deferred_operation<Impl> head,
     ASIO_MOVE_ARG(CompletionToken) tail)
   -> decltype(
-    ASIO_MOVE_OR_LVALUE(lazy_operation<Impl>)(head)(
+    ASIO_MOVE_OR_LVALUE(deferred_operation<Impl>)(head)(
         ASIO_MOVE_CAST(CompletionToken)(tail)))
 {
-  return ASIO_MOVE_OR_LVALUE(lazy_operation<Impl>)(head)(
+  return ASIO_MOVE_OR_LVALUE(deferred_operation<Impl>)(head)(
       ASIO_MOVE_CAST(CompletionToken)(tail));
 }
 
 template <typename T>
-struct is_lazy_operation : false_type
+struct is_deferred_operation : false_type
 {
 };
 
 template <typename Impl>
-struct is_lazy_operation<lazy_operation<Impl>> : true_type
+struct is_deferred_operation<deferred_operation<Impl>> : true_type
 {
 };
 
 /// A special value, similar to std::nothrow.
 /**
- * See the documentation for asio::experimental::lazy_t for a usage example.
+ * See the documentation for asio::experimental::deferred_t for a usage example.
  */
 #if defined(ASIO_HAS_CONSTEXPR) || defined(GENERATING_DOCUMENTATION)
-constexpr lazy_t lazy;
+constexpr deferred_t deferred;
 #elif defined(ASIO_MSVC)
-__declspec(selectany) lazy_t lazy;
+__declspec(selectany) deferred_t deferred;
 #endif
 
 } // namespace experimental
@@ -253,6 +253,6 @@ __declspec(selectany) lazy_t lazy;
 
 #include "asio/detail/pop_options.hpp"
 
-#include "asio/experimental/impl/lazy.hpp"
+#include "asio/experimental/impl/deferred.hpp"
 
-#endif // ASIO_EXPERIMENTAL_LAZY_HPP
+#endif // ASIO_EXPERIMENTAL_DEFERRED_HPP
--- asio/include/asio/experimental/impl/deferred.hpp
+++ asio/include/asio/experimental/impl/deferred.hpp
@@ -1,15 +1,15 @@
 //
-// experimental/impl/lazy.hpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~
+// experimental/impl/deferred.hpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// Copyright (c) 2003-2020 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Copyright (c) 2003-2021 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#ifndef ASIO_EXPERIMENTAL_IMPL_LAZY_HPP
-#define ASIO_EXPERIMENTAL_IMPL_LAZY_HPP
+#ifndef ASIO_EXPERIMENTAL_IMPL_DEFERRED_HPP
+#define ASIO_EXPERIMENTAL_IMPL_DEFERRED_HPP
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1200)
 # pragma once
@@ -27,7 +27,7 @@ namespace experimental {
 namespace detail {
 
 template <typename Signature, typename Initiation, typename... InitArgs>
-class lazy_init
+class deferred_init
 {
 private:
   typename decay<Initiation>::type initiation_;
@@ -45,7 +45,7 @@ private:
 
 public:
   template <typename I, typename... A>
-  explicit lazy_init(ASIO_MOVE_ARG(I) initiation,
+  explicit deferred_init(ASIO_MOVE_ARG(I) initiation,
       ASIO_MOVE_ARG(A)... init_args)
     : initiation_(ASIO_MOVE_CAST(I)(initiation)),
       init_args_(ASIO_MOVE_CAST(A)(init_args)...)
@@ -61,38 +61,38 @@ public:
   }
 };
 
-struct lazy_signature_probe {};
+struct deferred_signature_probe {};
 
 template <typename T>
-struct lazy_signature_probe_result
+struct deferred_signature_probe_result
 {
   typedef T type;
 };
 
 template <typename T>
-struct lazy_signature
+struct deferred_signature
 {
   typedef typename decltype(
-      declval<T>()(declval<lazy_signature_probe>()))::type type;
+      declval<T>()(declval<deferred_signature_probe>()))::type type;
 };
 
 template <typename HeadSignature, typename Tail>
-struct lazy_link_signature;
+struct deferred_link_signature;
 
 template <typename R, typename... Args, typename Tail>
-struct lazy_link_signature<R(Args...), Tail>
+struct deferred_link_signature<R(Args...), Tail>
 {
   typedef typename decltype(
       declval<Tail>()(declval<Args>()...)(
-        declval<lazy_signature_probe>()))::type type;
+        declval<deferred_signature_probe>()))::type type;
 };
 
 template <typename Handler, typename Tail>
-class lazy_link_handler
+class deferred_link_handler
 {
 public:
   template <typename H, typename T>
-  explicit lazy_link_handler(
+  explicit deferred_link_handler(
       ASIO_MOVE_ARG(H) handler, ASIO_MOVE_ARG(T) tail)
     : handler_(ASIO_MOVE_CAST(H)(handler)),
       tail_(ASIO_MOVE_CAST(T)(tail))
@@ -112,14 +112,14 @@ public:
   Tail tail_;
 };
 
-struct lazy_link_initiate
+struct deferred_link_initiate
 {
   template <typename Handler, typename Head, typename Tail>
   void operator()(ASIO_MOVE_ARG(Handler) handler,
       Head head, ASIO_MOVE_ARG(Tail) tail)
   {
     ASIO_MOVE_OR_LVALUE(Head)(head)(
-        lazy_link_handler<typename decay<Handler>::type,
+        deferred_link_handler<typename decay<Handler>::type,
           typename decay<Tail>::type>(
             ASIO_MOVE_CAST(Handler)(handler),
             ASIO_MOVE_CAST(Tail)(tail)));
@@ -127,15 +127,15 @@ struct lazy_link_initiate
 };
 
 template <typename Head, typename Tail>
-class lazy_link
+class deferred_link
 {
 public:
-  typedef typename lazy_link_signature<
-    typename lazy_signature<Head>::type, Tail>::type
+  typedef typename deferred_link_signature<
+    typename deferred_signature<Head>::type, Tail>::type
       signature;
 
   template <typename H, typename T>
-  explicit lazy_link(ASIO_MOVE_ARG(H) head, ASIO_MOVE_ARG(T) tail)
+  explicit deferred_link(ASIO_MOVE_ARG(H) head, ASIO_MOVE_ARG(T) tail)
     : head_(ASIO_MOVE_CAST(H)(head)),
       tail_(ASIO_MOVE_CAST(T)(tail))
   {
@@ -145,7 +145,7 @@ public:
   decltype(auto) operator()(ASIO_MOVE_ARG(CompletionToken) token)
   {
     return asio::async_initiate<CompletionToken, signature>(
-        lazy_link_initiate(), token,
+        deferred_link_initiate(), token,
         ASIO_MOVE_OR_LVALUE(Head)(head_),
         ASIO_MOVE_OR_LVALUE(Tail)(tail_));
   }
@@ -161,34 +161,34 @@ private:
 #if !defined(GENERATING_DOCUMENTATION)
 
 template <typename Signature>
-class async_result<experimental::lazy_t, Signature>
+class async_result<experimental::deferred_t, Signature>
 {
 public:
   template <typename Initiation, typename... InitArgs>
-  static experimental::lazy_operation<
-    experimental::detail::lazy_init<Signature, Initiation, InitArgs...> >
+  static experimental::deferred_operation<
+    experimental::detail::deferred_init<Signature, Initiation, InitArgs...> >
   initiate(ASIO_MOVE_ARG(Initiation) initiation,
-      experimental::lazy_t, ASIO_MOVE_ARG(InitArgs)... args)
+      experimental::deferred_t, ASIO_MOVE_ARG(InitArgs)... args)
   {
-    return experimental::lazy_operation<
-      experimental::detail::lazy_init<Signature, Initiation, InitArgs...> >(
-        experimental::detail::lazy_init<Signature, Initiation, InitArgs...>(
+    return experimental::deferred_operation<
+      experimental::detail::deferred_init<Signature, Initiation, InitArgs...> >(
+        experimental::detail::deferred_init<Signature, Initiation, InitArgs...>(
           ASIO_MOVE_CAST(Initiation)(initiation),
           ASIO_MOVE_CAST(InitArgs)(args)...));
     }
 };
 
 template <typename R, typename... Args>
-class async_result<experimental::detail::lazy_signature_probe, R(Args...)>
+class async_result<experimental::detail::deferred_signature_probe, R(Args...)>
 {
 public:
-  typedef experimental::detail::lazy_signature_probe_result<void(Args...)>
+  typedef experimental::detail::deferred_signature_probe_result<void(Args...)>
     return_type;
 
   template <typename Initiation, typename... InitArgs>
   static return_type initiate(
       ASIO_MOVE_ARG(Initiation),
-      experimental::detail::lazy_signature_probe,
+      experimental::detail::deferred_signature_probe,
       ASIO_MOVE_ARG(InitArgs)...)
   {
     return return_type{};
@@ -196,7 +196,7 @@ public:
 };
 
 template <typename Tail, typename R, typename... Args>
-  requires (experimental::is_lazy_operation<std::invoke_result_t<Tail, Args...>>::value)
+  requires (experimental::is_deferred_operation<std::invoke_result_t<Tail, Args...>>::value)
 class asio::async_result<Tail, R(Args...)>
 {
 public:
@@ -204,14 +204,14 @@ public:
   static auto initiate(ASIO_MOVE_ARG(Initiation) initiation,
       Tail tail, ASIO_MOVE_ARG(InitArgs)... init_args)
   {
-    return experimental::lazy_operation<
-      experimental::detail::lazy_link<
-        experimental::detail::lazy_init<R(Args...), Initiation, InitArgs...>,
+    return experimental::deferred_operation<
+      experimental::detail::deferred_link<
+        experimental::detail::deferred_init<R(Args...), Initiation, InitArgs...>,
         Tail> >(
-          experimental::detail::lazy_link<
-            experimental::detail::lazy_init<R(Args...), Initiation, InitArgs...>,
+          experimental::detail::deferred_link<
+            experimental::detail::deferred_init<R(Args...), Initiation, InitArgs...>,
             Tail>(
-              experimental::detail::lazy_init<R(Args...), Initiation, InitArgs...>(
+              experimental::detail::deferred_init<R(Args...), Initiation, InitArgs...>(
                 ASIO_MOVE_CAST(Initiation)(initiation),
                 ASIO_MOVE_CAST(InitArgs)(init_args)...),
               ASIO_MOVE_CAST(Tail)(tail)));
@@ -221,11 +221,11 @@ public:
 template <template <typename, typename> class Associator,
     typename Handler, typename Tail, typename DefaultCandidate>
 struct associator<Associator,
-    experimental::detail::lazy_link_handler<Handler, Tail>, DefaultCandidate>
+    experimental::detail::deferred_link_handler<Handler, Tail>, DefaultCandidate>
   : Associator<Handler, DefaultCandidate>
 {
   static typename Associator<Handler, DefaultCandidate>::type get(
-      const experimental::detail::lazy_link_handler<Handler, Tail>& h,
+      const experimental::detail::deferred_link_handler<Handler, Tail>& h,
       const DefaultCandidate& c = DefaultCandidate()) ASIO_NOEXCEPT
   {
     return Associator<Handler, DefaultCandidate>::get(h.handler_, c);
@@ -238,4 +238,4 @@ struct associator<Associator,
 
 #include "asio/detail/pop_options.hpp"
 
-#endif // ASIO_EXPERIMENTAL_IMPL_LAZY_HPP
+#endif // ASIO_EXPERIMENTAL_IMPL_DEFERRED_HPP
