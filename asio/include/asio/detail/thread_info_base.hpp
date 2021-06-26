2f1f52a9ff15b8a1aaf65e83e8b08e833623b196
Christopher Kohlhoff <chris@kohlhoff.com>
2021-06-27 00:00:12 +1000

    recyc

--- asio/include/asio/detail/thread_info_base.hpp
+++ asio/include/asio/detail/thread_info_base.hpp
@@ -73,7 +73,12 @@ public:
     enum
     {
       begin_mem_index = executor_function_tag::end_mem_index,
-      end_mem_index = begin_mem_index + 1
+#ifdef ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
+      end_mem_index =
+        begin_mem_index + ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
+#else // ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
+      end_mem_index = begin_mem_index + 2
+#endif // ASIO_RECYCLING_ALLOCATOR_CACHE_SIZE
     };
   };
 
