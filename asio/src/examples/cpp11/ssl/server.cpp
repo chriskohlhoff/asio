4a232888a87b7c6925f95576ced54e63454a24bf
Christopher Kohlhoff <chris@kohlhoff.com>
2021-11-12 08:15:40 +1100

    Regenerate certificates for ssl examples.

--- asio/src/examples/cpp11/ssl/server.cpp
+++ asio/src/examples/cpp11/ssl/server.cpp
@@ -88,7 +88,7 @@ public:
     context_.set_password_callback(std::bind(&server::get_password, this));
     context_.use_certificate_chain_file("server.pem");
     context_.use_private_key_file("server.pem", asio::ssl::context::pem);
-    context_.use_tmp_dh_file("dh2048.pem");
+    context_.use_tmp_dh_file("dh4096.pem");
 
     do_accept();
   }
