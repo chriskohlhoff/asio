d73ce69081bf04c5efd022b6ae3ee45361414b87
Christopher Kohlhoff <chris@kohlhoff.com>
2022-02-25 17:38:10 +1100

    Mark experimental::as_single() result as nodiscard.

--- asio/include/asio/experimental/as_single.hpp
+++ asio/include/asio/experimental/as_single.hpp
@@ -118,7 +118,8 @@ public:
 /// Create a completion token to specify that the completion handler arguments
 /// should be combined into a single argument.
 template <typename CompletionToken>
-inline ASIO_CONSTEXPR as_single_t<typename decay<CompletionToken>::type>
+ASIO_NODISCARD inline
+ASIO_CONSTEXPR as_single_t<typename decay<CompletionToken>::type>
 as_single(ASIO_MOVE_ARG(CompletionToken) completion_token)
 {
   return as_single_t<typename decay<CompletionToken>::type>(
