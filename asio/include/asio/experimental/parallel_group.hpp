0bbe464963089afc1114622eb600aec974e1768f
Christopher Kohlhoff <chris@kohlhoff.com>
2022-02-25 17:40:31 +1100

    Mark experimental::make_parallel_group() result as nodiscard.

--- asio/include/asio/experimental/parallel_group.hpp
+++ asio/include/asio/experimental/parallel_group.hpp
@@ -200,7 +200,8 @@ private:
  * @endcode
  */
 template <typename... Ops>
-inline parallel_group<Ops...> make_parallel_group(Ops... ops)
+ASIO_NODISCARD inline parallel_group<Ops...>
+make_parallel_group(Ops... ops)
 {
   return parallel_group<Ops...>(std::move(ops)...);
 }
