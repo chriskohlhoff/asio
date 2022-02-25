bace22412ffbce1ac2265cf1403d6253ff5ad04d
Christopher Kohlhoff <chris@kohlhoff.com>
2022-02-25 12:19:15 +1100

    Add missing include of asio/error_code.hpp.

--- asio/include/asio/experimental/detail/channel_send_functions.hpp
+++ asio/include/asio/experimental/detail/channel_send_functions.hpp
@@ -18,6 +18,7 @@
 #include "asio/detail/config.hpp"
 #include "asio/async_result.hpp"
 #include "asio/detail/type_traits.hpp"
+#include "asio/error_code.hpp"
 #include "asio/experimental/detail/channel_message.hpp"
 
 #include "asio/detail/push_options.hpp"
