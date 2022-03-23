42026cdd6bd3428a31a5e27e539e80291a9719ae
Christopher Kohlhoff <chris@kohlhoff.com>
2022-03-23 11:38:31 +1100

    Update platform macros documentation.

--- asio/include/asio/detail/config.hpp
+++ asio/include/asio/detail/config.hpp
@@ -38,6 +38,7 @@
 # define ASIO_DISABLE_BOOST_THROW_EXCEPTION 1
 # define ASIO_DISABLE_BOOST_WORKAROUND 1
 #else // defined(ASIO_STANDALONE)
+// Boost.Config library is available.
 # include <boost/config.hpp>
 # include <boost/version.hpp>
 # define ASIO_HAS_BOOST_CONFIG 1
