79e97f06cb9582e3376e33c7b0b7c60d7cfe5983
Christopher Kohlhoff <chris@kohlhoff.com>
2022-07-05 18:09:15 +1000

    Add coroutines examples for C++20.

--- asio/boostify.pl
+++ asio/boostify.pl
@@ -597,6 +597,7 @@ sub copy_examples
       "src/examples/cpp14/parallel_group",
       "src/examples/cpp17/coroutines_ts",
       "src/examples/cpp20/channels",
+      "src/examples/cpp20/coroutines",
       "src/examples/cpp20/operations");
 
   our $boost_dir;
