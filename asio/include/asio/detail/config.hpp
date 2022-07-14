6d51490576ef219f3bc6dda3baecc3a9efe49dd5
Christopher Kohlhoff <chris@kohlhoff.com>
2022-07-14 11:08:35 +1000

    Make standard library feature macros available.

--- asio/include/asio/detail/config.hpp
+++ asio/include/asio/detail/config.hpp
@@ -24,6 +24,17 @@
 # endif // !defined(ASIO_ENABLE_BOOST)
 #endif // !defined(ASIO_STANDALONE)
 
+// Make standard library feature macros available.
+#if defined(__has_include)
+# if __has_include(<version>)
+#  include <version>
+# else // __has_include(<version>)
+#  include <cstddef>
+# endif // __has_include(<version>)
+#else // defined(__has_include)
+# include <cstddef>
+#endif // defined(__has_include)
+
 // boostify: non-boost code ends here
 #if defined(ASIO_STANDALONE)
 # define ASIO_DISABLE_BOOST_ALIGN 1
