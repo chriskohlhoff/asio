e190fe8669972d91ddf57935e17c0e5bfd34a961
Christopher Kohlhoff <chris@kohlhoff.com>
2022-07-31 20:28:30 +1000

    Store reference to basic_yield_context in handler.
    Improves performance by not copying the executor into the handler.

--- asio/include/asio/impl/spawn.hpp
+++ asio/include/asio/impl/spawn.hpp
@@ -387,9 +387,8 @@ public:
   typedef cancellation_slot cancellation_slot_type;
 
   spawn_handler_base(const basic_yield_context<Executor>& yield)
-    : executor_(yield.executor_),
-      spawned_thread_(yield.spawned_thread_),
-      ec_(yield.ec_)
+    : yield_(yield),
+      spawned_thread_(yield.spawned_thread_)
   {
     spawned_thread_->detach();
 #if !defined(ASIO_HAS_MOVE)
@@ -400,20 +399,18 @@ public:
 #if defined(ASIO_HAS_MOVE)
 
   spawn_handler_base(spawn_handler_base&& other) ASIO_NOEXCEPT
-    : executor_(ASIO_MOVE_CAST(Executor)(other.executor_)),
-      spawned_thread_(other.spawned_thread_),
-      ec_(other.ec_)
+    : yield_(other.yield_),
+      spawned_thread_(other.spawned_thread_)
+
   {
     other.spawned_thread_ = 0;
-    other.ec_ = 0;
   }
 
 #else // defined(ASIO_HAS_MOVE)
 
   spawn_handler_base(const spawn_handler_base& other) ASIO_NOEXCEPT
-    : executor_(other.executor_),
-      spawned_thread_(other.spawned_thread_),
-      ec_(other.ec_)
+    : yield_(other.yield_),
+      spawned_thread_(other.spawned_thread_)
   {
     spawned_thread_->detach();
     spawned_thread_->attach(&spawned_thread_);
@@ -424,12 +421,12 @@ public:
   ~spawn_handler_base()
   {
     if (spawned_thread_)
-      (post)(executor_, spawned_thread_destroyer(spawned_thread_));
+      (post)(yield_.executor_, spawned_thread_destroyer(spawned_thread_));
   }
 
   executor_type get_executor() const ASIO_NOEXCEPT
   {
-    return executor_;
+    return yield_.executor_;
   }
 
   cancellation_slot_type get_cancellation_slot() const ASIO_NOEXCEPT
@@ -445,9 +442,8 @@ public:
   }
 
 protected:
-  Executor executor_;
+  const basic_yield_context<Executor>& yield_;
   spawned_thread_base* spawned_thread_;
-  asio::error_code* ec_;
 };
 
 // Completion handlers for when basic_yield_context is used as a token.
@@ -494,9 +490,9 @@ public:
 
   void operator()(asio::error_code ec)
   {
-    if (this->ec_)
+    if (this->yield_.ec_)
     {
-      *this->ec_ = ec;
+      *this->yield_.ec_ = ec;
       result_ = 0;
     }
     else
@@ -594,9 +590,9 @@ public:
 
   void operator()(asio::error_code ec, T value)
   {
-    if (this->ec_)
+    if (this->yield_.ec_)
     {
-      *this->ec_ = ec;
+      *this->yield_.ec_ = ec;
       result_.ec_ = 0;
     }
     else
@@ -712,9 +708,9 @@ public:
       ASIO_MOVE_ARG(Args)... args)
   {
     return_type value(ASIO_MOVE_CAST(Args)(args)...);
-    if (this->ec_)
+    if (this->yield_.ec_)
     {
-      *this->ec_ = ec;
+      *this->yield_.ec_ = ec;
       result_.ec_ = 0;
     }
     else
