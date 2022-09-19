22761b822cd108c6d36307c4a3c1b00900537ef3
Klemens <klemens.d.morgenstern@gmail.com>
2022-09-19 12:40:20 +0800

    Added allow_recursion.

--- asio/include/asio/early_complete.hpp
+++ asio/include/asio/early_complete.hpp
@@ -15,6 +15,8 @@
 
 #include "asio/detail/config.hpp"
 
+#include "asio/associator.hpp"
+#include "asio/async_result.hpp"
 #include "asio/detail/type_traits.hpp"
 
 #include "asio/detail/push_options.hpp"
@@ -31,50 +33,141 @@ struct early_completion_probe;
 template<typename ... Args>
 struct early_completion_probe<void(Args...)>
 {
-  void operator()(ASIO_MOVE_ARG(Args) ... );
+  void operator()(ASIO_MOVE_ARG(Args) ... ) const;
 };
 
 }
 
-template<typename Signature, typename Initiation, typename = void>
-struct has_early_completion : false_type
+template<typename Initiation, typename Signature,
+         typename = void>
+struct has_early_completion_step : false_type
 {
 };
 
-template<typename Signature, typename Initiation>
-struct has_early_completion<
-        Signature, Initiation,
-        decltype(std::declval<Initiation>().complete_early(
-                std::declval<detail::early_completion_probe<Signature>>())
-                )> : true_type
+template<typename Initiation, typename Signature>
+struct has_early_completion_step<
+        Initiation, Signature,
+        decltype(declval<Initiation>().complete_early(declval<detail::early_completion_probe<Signature>>()))> : true_type
 {
 };
+
+template<typename Initiation, typename ... Signatures>
+struct has_early_completion
+    : conjunction<has_early_completion_step<Initiation, Signatures>...>
+{
+};
+
 
 namespace detail
 {
 
-template<typename Signature, typename Initiation, typename Handler>
-ASIO_CONSTEXPR bool invoke_early_completion_impl(ASIO_MOVE_ARG(Initiation), ASIO_MOVE_ARG(Handler), false_type)
+template<typename Initiation, typename Handler>
+ASIO_CONSTEXPR void invoke_early_completion_impl(ASIO_MOVE_ARG(Initiation), ASIO_MOVE_ARG(Handler), false_type)
+{
+}
+
+template<typename Initiation, typename Handler>
+void invoke_early_completion_impl(ASIO_MOVE_ARG(Initiation) init, ASIO_MOVE_ARG(Handler) handler, true_type)
+{
+  ASIO_MOVE_CAST(Initiation)(init).complete_early(ASIO_MOVE_CAST(Handler)(handler));
+}
+
+template<typename Initiation>
+ASIO_CONSTEXPR bool check_early_completion_impl(ASIO_MOVE_ARG(Initiation), false_type)
 {
   return false;
 }
 
-template<typename Signature, typename Initiation, typename Handler>
-bool invoke_early_completion_impl(ASIO_MOVE_ARG(Initiation) init, ASIO_MOVE_ARG(Handler) handler, true_type)
+template<typename Initiation>
+bool check_early_completion_impl(ASIO_MOVE_ARG(Initiation) init, true_type)
 {
-  return ASIO_MOVE_CAST(Initiation)(init).complete_early(ASIO_MOVE_CAST(Handler)(handler));
+  return ASIO_MOVE_CAST(Initiation)(init).can_complete_early();
 }
 
 }
 
-template<typename Signature, typename Initiation, typename Handler>
-bool invoke_early_completion(ASIO_MOVE_ARG(Initiation) init, ASIO_MOVE_ARG(Handler) handler)
+template<typename ... Signatures, typename Initiation, typename Handler>
+void invoke_early_completion(ASIO_MOVE_ARG(Initiation) init, ASIO_MOVE_ARG(Handler) handler)
 {
-  return detail::invoke_early_completion_impl(
+  detail::invoke_early_completion_impl(
           ASIO_MOVE_CAST(Initiation)(init),
           ASIO_MOVE_CAST(Handler)(handler),
-          has_early_completion<Signature, typename std::decay<Initiation>::type>{});
+          has_early_completion<typename decay<Initiation>::type, Signatures...>{});
 }
+
+template<typename ... Signatures, typename Initiation>
+bool check_early_completion(ASIO_MOVE_ARG(Initiation) init)
+{
+  return detail::check_early_completion_impl(
+          ASIO_MOVE_CAST(Initiation)(init),
+          has_early_completion<typename decay<Initiation>::type, Signatures...>{});
+}
+
+
+
+template<typename Token>
+struct allow_recursion_t
+{
+  template<typename T>
+  allow_recursion_t(ASIO_MOVE_ARG(T) token) : token(ASIO_MOVE_CAST(T)(token)) {}
+  Token token;
+};
+
+template<typename Token>
+allow_recursion_t<typename decay<Token>::type> allow_recursion(ASIO_MOVE_ARG(Token) token)
+{
+  return allow_recursion_t<typename decay<Token>::type>
+          (
+              ASIO_MOVE_CAST(Token)(token)
+          );
+}
+
+
+namespace detail
+{
+
+template<typename Initiation>
+struct immediate_initiate
+{
+  Initiation init;
+  template<typename Init_>
+  immediate_initiate(ASIO_MOVE_ARG(Init_) init) : init(ASIO_MOVE_CAST(Init_)(init)) {}
+
+  template<typename Handler>
+  void operator()(ASIO_MOVE_ARG(Handler) handler)
+  {
+    invoke_early_completion(ASIO_MOVE_OR_LVALUE(Initiation)(init),
+                            ASIO_MOVE_CAST(Handler)(handler));
+  }
+
+};
+
+}
+
+template <typename Init, typename... Signatures>
+struct async_result<allow_recursion_t<Init>, Signatures ...>
+{
+  typedef typename async_result<Init, Signatures...>::return_type return_type;
+
+
+  template <typename Initiation, typename RawCompletionToken, typename... InitArgs>
+  static return_type initiate(ASIO_MOVE_ARG(Initiation) init,
+                              ASIO_MOVE_ARG(RawCompletionToken) wrapped_token,
+                              ASIO_MOVE_ARG(InitArgs)... init_args)
+  {
+    if (check_early_completion<Signatures...>(ASIO_MOVE_CAST(Initiation)(init)))
+        return async_result<Init, Signatures...>::initiate(
+                detail::immediate_initiate<Initiation>(ASIO_MOVE_CAST(Initiation)(init)),
+                ASIO_MOVE_CAST(Init)(wrapped_token.token),
+                ASIO_MOVE_CAST(InitArgs)(init_args)...);
+
+    return async_result<Init, Signatures...>::initiate(
+            ASIO_MOVE_CAST(Initiation)(init),
+            ASIO_MOVE_CAST(Init)(wrapped_token.token),
+            ASIO_MOVE_CAST(InitArgs)(init_args)...);
+  }
+};
+
 
 }
 
--- asio/src/tests/unit/early_complete.cpp
+++ asio/src/tests/unit/early_complete.cpp
@@ -10,6 +10,7 @@
 
 // Test that header file is self-contained.
 #include "asio/early_complete.hpp"
+#include "asio/steady_timer.hpp"
 
 #include "./unit_test.hpp"
 
@@ -18,18 +19,48 @@ struct dummy_init
   template<typename Func>
   auto complete_early(Func && func) -> decltype(std::declval<Func>()(int()));
 
+  bool can_complete_early();
+
 };
 
 void trait_test()
 {
-  using t1 = asio::has_early_completion<void(), dummy_init>;
-  using t2 = asio::has_early_completion<void(int), dummy_init>;
+  using t1 = asio::has_early_completion<dummy_init, void()>;
+  using t2 = asio::has_early_completion<dummy_init, void(int)>;
   ASIO_CHECK(!t1::value);
   ASIO_CHECK(t2::value);
+}
+
+
+void timer_test()
+{
+  asio::io_context ctx;
+  asio::steady_timer tim{ctx};
+
+  // this thing doesn't dispatch, we're just completing immediately
+
+  bool done = false;
+  auto cpl =  [&](asio::error_code ec)
+              {
+                ASIO_CHECK(!ec);
+                done = true;
+              };
+
+  tim.async_wait(asio::allow_recursion(cpl));
+  ASIO_CHECK(done);
+  done = false;
+
+  tim.expires_after(std::chrono::milliseconds(50));
+  tim.async_wait(asio::allow_recursion(cpl));
+  ASIO_CHECK(!done);
+  ctx.run();
+  ASIO_CHECK(done);
+
 }
 
 ASIO_TEST_SUITE
 (
     "early_completion",
     ASIO_TEST_CASE(trait_test)
+    ASIO_TEST_CASE(timer_test)
 )
