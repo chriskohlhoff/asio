1e2fca858e847ba3dc2e9f69e68ae2b2cc097a4e
Christopher Kohlhoff <chris@kohlhoff.com>
2022-11-03 16:28:50 +1100

    Add any_completion_handler.hpp to convenience header.

--- asio/include/asio.hpp
+++ asio/include/asio.hpp
@@ -16,6 +16,7 @@
 #endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
 
 #include "asio/any_completion_executor.hpp"
+#include "asio/any_completion_handler.hpp"
 #include "asio/any_io_executor.hpp"
 #include "asio/append.hpp"
 #include "asio/as_tuple.hpp"
