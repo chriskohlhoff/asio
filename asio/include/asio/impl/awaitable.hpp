d0ce27643fb002b2a9d66d4d5e24dff86407bb18
Christopher Kohlhoff <chris@kohlhoff.com>
2024-04-02 19:36:43 +1100

    Launder awaitable's result storage.

--- asio/include/asio/impl/awaitable.hpp
+++ asio/include/asio/impl/awaitable.hpp
@@ -538,7 +538,7 @@ public:
   ~awaitable_frame()
   {
     if (has_result_)
-      static_cast<T*>(static_cast<void*>(result_))->~T();
+      std::launder(static_cast<T*>(static_cast<void*>(result_)))->~T();
   }
 
   awaitable<T, Executor> get_return_object() noexcept
@@ -564,7 +564,8 @@ public:
   {
     this->caller_ = nullptr;
     this->rethrow_exception();
-    return std::move(*static_cast<T*>(static_cast<void*>(result_)));
+    return std::move(*std::launder(
+          static_cast<T*>(static_cast<void*>(result_))));
   }
 
 private:
