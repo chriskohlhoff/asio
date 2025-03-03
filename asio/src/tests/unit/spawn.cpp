9c7db9d88ce6ec8913f671c4da9fa2cbb64ca0ba
Christopher Kohlhoff <chris@kohlhoff.com>
2025-03-04 09:03:19 +1100

    Update copyright notices.

--- asio/src/tests/unit/spawn.cpp
+++ asio/src/tests/unit/spawn.cpp
@@ -2,7 +2,7 @@
 // spawn.cpp
 // ~~~~~~~~~
 //
-// Copyright (c) 2003-2024 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Copyright (c) 2003-2025 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 // Copyright (c) 2024 Casey Bodley (cbodley at redhat dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
