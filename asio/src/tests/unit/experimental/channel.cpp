aaecb10b2781c44bfcae8e7c34f0d14d04119f9a
Christopher Kohlhoff <chris@kohlhoff.com>
2025-08-04 22:12:38 +1000

    Fix compilation errors in channel<void(error_code)>.

--- asio/src/tests/unit/experimental/channel.cpp
+++ asio/src/tests/unit/experimental/channel.cpp
@@ -863,6 +863,19 @@ void channel_with_any_completion_handler_test()
   ASIO_CHECK(!ec2);
 }
 
+void channel_move_test()
+{
+  io_context ctx;
+
+  channel<void(asio::error_code)> ch1(ctx);
+  channel<void(asio::error_code)> ch2 = std::move(ch1);
+  (void)ch2;
+
+  channel<void(asio::error_code, std::string)> ch3(ctx);
+  channel<void(asio::error_code, std::string)> ch4 = std::move(ch3);
+  (void)ch4;
+}
+
 ASIO_TEST_SUITE
 (
   "experimental/channel",
@@ -885,4 +898,5 @@ ASIO_TEST_SUITE
   ASIO_TEST_CASE(try_send_n_via_dispatch)
   ASIO_TEST_CASE(implicit_error_signature_channel_test)
   ASIO_TEST_CASE(channel_with_any_completion_handler_test)
+  ASIO_COMPILE_TEST_CASE(channel_move_test)
 )
